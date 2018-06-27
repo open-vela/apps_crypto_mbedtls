cbcfda497ba812c39a75c2118a45537ed99c46a4
liujinye <liujinye@xiaomi.com>
2018-06-28 02:14:00 +0800

    fix if condition to validate encrypt key usage

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -487,7 +487,6 @@ psa_status_t psa_import_key( psa_key_slot_t key,
     {
         int ret;
         mbedtls_pk_context pk;
-        psa_status_t status = PSA_SUCCESS;
         mbedtls_pk_init( &pk );
         if( PSA_KEY_TYPE_IS_KEYPAIR( type ) )
             ret = mbedtls_pk_parse_key( &pk, data, data_length, NULL, 0 );
@@ -503,7 +502,7 @@ psa_status_t psa_import_key( psa_key_slot_t key,
                     type == PSA_KEY_TYPE_RSA_KEYPAIR )
                     slot->data.rsa = mbedtls_pk_rsa( pk );
                 else
-                    status = PSA_ERROR_INVALID_ARGUMENT;
+                    return( PSA_ERROR_INVALID_ARGUMENT );
                 break;
 #endif /* MBEDTLS_RSA_C */
 #if defined(MBEDTLS_ECP_C)
@@ -516,26 +515,15 @@ psa_status_t psa_import_key( psa_key_slot_t key,
                     psa_ecc_curve_t expected_curve =
                         PSA_KEY_TYPE_GET_CURVE( type );
                     if( actual_curve != expected_curve )
-                    {
-                        status = PSA_ERROR_INVALID_ARGUMENT;
-                        break;
-                    }
+                        return( PSA_ERROR_INVALID_ARGUMENT );
                     slot->data.ecp = ecp;
                 }
                 else
-                    status = PSA_ERROR_INVALID_ARGUMENT;
+                    return( PSA_ERROR_INVALID_ARGUMENT );
                 break;
 #endif /* MBEDTLS_ECP_C */
             default:
-                status = PSA_ERROR_INVALID_ARGUMENT;
-                break;
-        }
-        /* Free the content of the pk object only on error. On success,
-         * the content of the object has been stored in the slot. */
-        if( status != PSA_SUCCESS )
-        {
-            mbedtls_pk_free( &pk );
-            return( status );
+                return( PSA_ERROR_INVALID_ARGUMENT );
         }
     }
     else
