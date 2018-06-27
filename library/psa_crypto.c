c0174731a8ea55f7630e3ba136b688478e91cff9
liujinye <liujinye@xiaomi.com>
2018-06-27 22:57:11 +0800

    Correct reference for RSA keypair export format

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -2513,7 +2513,7 @@ psa_status_t psa_get_key_lifetime( psa_key_slot_t key,
 }
 
 psa_status_t psa_set_key_lifetime( psa_key_slot_t key,
-                                   psa_key_lifetime_t lifetime )
+                                   const psa_key_lifetime_t lifetime )
 {
     key_slot_t *slot;
 
