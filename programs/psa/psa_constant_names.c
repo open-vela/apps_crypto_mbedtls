0b0d250d5c7e3ba056442d89cc58fc8dd7fdd666
openvela-robot <openvela-robot@xiaomi.com>
2018-10-23 13:15:01 +0800

    Merge pull request #165 from ARMmbed/dev/dgreen-arm/windows_proj_wip
    Windows build fixes (#165)

--- programs/psa/psa_constant_names.c
+++ programs/psa/psa_constant_names.c
@@ -50,6 +50,16 @@ static void append(char **buffer, size_t buffer_size,
     }
 }
 
+static void append_integer(char **buffer, size_t buffer_size,
+                           size_t *required_size,
+                           const char *format /*printf format for value*/,
+                           unsigned long value)
+{
+    size_t n = snprintf(*buffer, buffer_size - *required_size, format, value);
+    if (n < buffer_size - *required_size) *buffer += n;
+    *required_size += n;
+}
+
 /* The code of these function is automatically generated and included below. */
 static const char *psa_ecc_curve_name(psa_ecc_curve_t curve);
 static const char *psa_hash_algorithm_name(psa_algorithm_t hash_alg);
@@ -66,10 +76,8 @@ static void append_with_curve(char **buffer, size_t buffer_size,
         append(buffer, buffer_size, required_size,
                curve_name, strlen(curve_name));
     } else {
-        size_t n = snprintf(*buffer, buffer_size - *required_size,
-                            "0x%04x", (unsigned) curve);
-        if (n < buffer_size - *required_size) *buffer += n;
-        *required_size += n;
+        append_integer(buffer, buffer_size, required_size,
+                       "0x%04x", curve);
     }
     append(buffer, buffer_size, required_size, ")", 1);
 }
@@ -86,10 +94,8 @@ static void append_with_hash(char **buffer, size_t buffer_size,
         append(buffer, buffer_size, required_size,
                hash_name, strlen(hash_name));
     } else {
-        size_t n = snprintf(*buffer, buffer_size - *required_size,
-                            "0x%08lx", (unsigned long) hash_alg);
-        if (n < buffer_size - *required_size) *buffer += n;
-        *required_size += n;
+        append_integer(buffer, buffer_size, required_size,
+                       "0x%08lx", hash_alg);
     }
     append(buffer, buffer_size, required_size, ")", 1);
 }
@@ -106,9 +112,9 @@ static int psa_snprint_status(char *buffer, size_t buffer_size,
         size_t length = strlen(name);
         if (length < buffer_size) {
             memcpy(buffer, name, length + 1);
-            return length;
+            return (int) length;
         } else {
-            return buffer_size;
+            return (int) buffer_size;
         }
     }
 }
@@ -123,9 +129,9 @@ static int psa_snprint_ecc_curve(char *buffer, size_t buffer_size,
         size_t length = strlen(name);
         if (length < buffer_size) {
             memcpy(buffer, name, length + 1);
-            return length;
+            return (int) length;
         } else {
-            return buffer_size;
+            return (int) buffer_size;
         }
     }
 }
@@ -136,9 +142,9 @@ static void usage(const char *program_name)
            program_name == NULL ? "psa_constant_names" : program_name);
     printf("Print the symbolic name whose numerical value is VALUE in TYPE.\n");
     printf("Supported types (with = between aliases):\n");
-    printf("  alg=algorithm         Status code (psa_algorithm_t)\n");
+    printf("  alg=algorithm         Algorithm (psa_algorithm_t)\n");
     printf("  curve=ecc_curve       Elliptic curve identifier (psa_ecc_curve_t)\n");
-    printf("  type=key_type         Status code (psa_key_type_t)\n");
+    printf("  type=key_type         Key type (psa_key_type_t)\n");
     printf("  usage=key_usage       Key usage (psa_key_usage_t)\n");
     printf("  error=status          Status code (psa_status_t)\n");
 }
@@ -167,15 +173,15 @@ int main(int argc, char *argv[])
     }
 
     if (!strcmp(argv[1], "error") || !strcmp(argv[1], "status"))
-        psa_snprint_status(buffer, sizeof(buffer), value);
+        psa_snprint_status(buffer, sizeof(buffer), (psa_status_t) value);
     else if (!strcmp(argv[1], "alg") || !strcmp(argv[1], "algorithm"))
-        psa_snprint_algorithm(buffer, sizeof(buffer), value);
+        psa_snprint_algorithm(buffer, sizeof(buffer), (psa_algorithm_t) value);
     else if (!strcmp(argv[1], "curve") || !strcmp(argv[1], "ecc_curve"))
-        psa_snprint_ecc_curve(buffer, sizeof(buffer), value);
+        psa_snprint_ecc_curve(buffer, sizeof(buffer), (psa_ecc_curve_t) value);
     else if (!strcmp(argv[1], "type") || !strcmp(argv[1], "key_type"))
-        psa_snprint_key_type(buffer, sizeof(buffer), value);
+        psa_snprint_key_type(buffer, sizeof(buffer), (psa_key_type_t) value);
     else if (!strcmp(argv[1], "usage") || !strcmp(argv[1], "key_usage"))
-        psa_snprint_key_usage(buffer, sizeof(buffer), value);
+        psa_snprint_key_usage(buffer, sizeof(buffer), (psa_key_usage_t) value);
     else {
         printf("Unknown type: %s\n", argv[1]);
         return EXIT_FAILURE;
