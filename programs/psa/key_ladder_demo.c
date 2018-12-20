f05701510235b7b0f32a3a9bc555b6d729e0d5e2
openvela-robot <openvela-robot@xiaomi.com>
2018-12-20 17:44:21 +0800

    programs/Makefile: List all programs one by one
    This makes it easier to add or remove programs as well as see which
    programs were added or removed in diffs.

--- programs/psa/key_ladder_demo.c
+++ programs/psa/key_ladder_demo.c
@@ -66,6 +66,7 @@
 #define mbedtls_calloc       calloc
 #define mbedtls_free         free
 #define mbedtls_printf       printf
+#define mbedtls_exit         exit
 #endif
 #include <stdio.h>
 #include <string.h>
@@ -618,11 +619,23 @@ static void usage( void )
     mbedtls_printf( "                    and the same sequence of labels.\n" );
 }
 
+#if defined(MBEDTLS_CHECK_PARAMS)
+#include "mbedtls/platform_util.h"
+void mbedtls_param_failed( const char *failure_condition,
+                           const char *file,
+                           int line )
+{
+    mbedtls_printf( "%s:%i: Input param failed - %s\n",
+                    file, line, failure_condition );
+    mbedtls_exit( MBEDTLS_EXIT_FAILURE );
+}
+#endif
+
 int main( int argc, char *argv[] )
 {
-    char *key_file_name = "master.key";
-    char *input_file_name = NULL;
-    char *output_file_name = NULL;
+    const char *key_file_name = "master.key";
+    const char *input_file_name = NULL;
+    const char *output_file_name = NULL;
     const char *ladder[MAX_LADDER_DEPTH];
     size_t ladder_depth = 0;
     int i;
