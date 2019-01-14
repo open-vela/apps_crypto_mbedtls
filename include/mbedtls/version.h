056361bc48d27e187a63c12048188d45a7963e06
openvela-robot <openvela-robot@xiaomi.com>
2019-01-14 10:47:20 +0800

    Merge pull request #7 from gilles-peskine-arm/all_sh-mbedcrypto
    [mbedcrypto] all.sh: make it possible to run a subset of the components

--- include/mbedtls/version.h
+++ include/mbedtls/version.h
@@ -39,17 +39,17 @@
  * Major, Minor, Patchlevel
  */
 #define MBEDTLS_VERSION_MAJOR  2
-#define MBEDTLS_VERSION_MINOR  15
-#define MBEDTLS_VERSION_PATCH  1
+#define MBEDTLS_VERSION_MINOR  14
+#define MBEDTLS_VERSION_PATCH  0
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define MBEDTLS_VERSION_NUMBER         0x020F0100
-#define MBEDTLS_VERSION_STRING         "2.15.1"
-#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.15.1"
+#define MBEDTLS_VERSION_NUMBER         0x020E0000
+#define MBEDTLS_VERSION_STRING         "2.14.0"
+#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.14.0"
 
 #if defined(MBEDTLS_VERSION_C)
 
