0f1b6161796ed37ccd9b954d98a026546fc6455c
openvela-robot <openvela-robot@xiaomi.com>
2019-01-28 15:18:15 +0800

    Increase okm_hex buffer to contain null character
    Add an additional byte for the `okm_hex` buffer, to assure
    it is null-terminated in case `okm` is 128 bytes long.

--- include/mbedtls/version.h
+++ include/mbedtls/version.h
@@ -39,7 +39,7 @@
  * Major, Minor, Patchlevel
  */
 #define MBEDTLS_VERSION_MAJOR  2
-#define MBEDTLS_VERSION_MINOR  15
+#define MBEDTLS_VERSION_MINOR  13
 #define MBEDTLS_VERSION_PATCH  1
 
 /**
@@ -47,9 +47,9 @@
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define MBEDTLS_VERSION_NUMBER         0x020F0100
-#define MBEDTLS_VERSION_STRING         "2.15.1"
-#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.15.1"
+#define MBEDTLS_VERSION_NUMBER         0x020D0100
+#define MBEDTLS_VERSION_STRING         "2.13.1"
+#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.13.1"
 
 #if defined(MBEDTLS_VERSION_C)
 
