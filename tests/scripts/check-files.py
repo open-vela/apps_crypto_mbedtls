e64991af16ac0919fff6fb353b43aeb8fb3709c5
openvela-robot <openvela-robot@xiaomi.com>
2019-01-28 13:29:01 +0800

    Improve the description of PSA_ALG_ANY_HASH
    Make it clearer what PSA_ALG_ANY_HASH can and cannot be used for.

--- tests/scripts/check-files.py
+++ tests/scripts/check-files.py
@@ -138,9 +138,7 @@ class TodoIssueTracker(IssueTracker):
         super().__init__()
         self.heading = "TODO present:"
         self.files_exemptions = [
-            os.path.basename(__file__),
-            "benchmark.c",
-            "pull_request_template.md",
+            __file__, "benchmark.c", "pull_request_template.md"
         ]
 
     def issue_with_line(self, line):
