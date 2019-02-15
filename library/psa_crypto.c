d5cdf7478a91c1a71e05039c9274546f111cc74e
Jaeden Amero <jaeden.amero@arm.com>
2019-02-15 13:52:25 +0000

    psa: Disallow use of invalid hash contexts
    If a hash context has not been set up, fail with PSA_ERROR_BAD_STATE as
    documented in crypto.h and the PSA Crypto specification.

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -1496,8 +1496,7 @@ psa_status_t psa_hash_update( psa_hash_operation_t *operation,
             break;
 #endif
         default:
-            ret = MBEDTLS_ERR_MD_BAD_INPUT_DATA;
-            break;
+            return( PSA_ERROR_BAD_STATE );
     }
 
     if( ret != 0 )
@@ -1569,8 +1568,7 @@ psa_status_t psa_hash_finish( psa_hash_operation_t *operation,
             break;
 #endif
         default:
-            ret = MBEDTLS_ERR_MD_BAD_INPUT_DATA;
-            break;
+            return( PSA_ERROR_BAD_STATE );
     }
     status = mbedtls_to_psa_error( ret );
 
