6d18414929fc75fa490dc5ada152a69382328c74
openvela-robot <openvela-robot@xiaomi.com>
2019-02-19 16:59:33 +0800

    Extend abi-checking to different repos

--- scripts/abi_check.py
+++ scripts/abi_check.py
@@ -26,15 +26,28 @@ import tempfile
 
 
 class AbiChecker(object):
+    """API and ABI checker."""
 
-    def __init__(self, report_dir, old_rev, new_rev, keep_all_reports):
+    def __init__(self, report_dir, old_repo, old_rev, new_repo, new_rev,
+                 keep_all_reports):
+        """Instantiate the API/ABI checker.
+
+        report_dir: directory for output files
+        old_repo: repository for git revision to compare against
+        old_rev: reference git revision to compare against
+        new_repo: repository for git revision to check
+        new_rev: git revision to check
+        keep_all_reports: if false, delete old reports
+        """
         self.repo_path = "."
         self.log = None
         self.setup_logger()
         self.report_dir = os.path.abspath(report_dir)
         self.keep_all_reports = keep_all_reports
         self.should_keep_report_dir = os.path.isdir(self.report_dir)
+        self.old_repo = old_repo
         self.old_rev = old_rev
+        self.new_repo = new_repo
         self.new_rev = new_rev
         self.mbedtls_modules = ["libmbedcrypto", "libmbedtls", "libmbedx509"]
         self.old_dumps = {}
@@ -42,7 +55,8 @@ class AbiChecker(object):
         self.git_command = "git"
         self.make_command = "make"
 
-    def check_repo_path(self):
+    @staticmethod
+    def check_repo_path():
         current_dir = os.path.realpath('.')
         root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         if current_dir != root_dir:
@@ -53,18 +67,41 @@ class AbiChecker(object):
         self.log.setLevel(logging.INFO)
         self.log.addHandler(logging.StreamHandler())
 
-    def check_abi_tools_are_installed(self):
+    @staticmethod
+    def check_abi_tools_are_installed():
         for command in ["abi-dumper", "abi-compliance-checker"]:
             if not shutil.which(command):
                 raise Exception("{} not installed, aborting".format(command))
 
-    def get_clean_worktree_for_git_revision(self, git_rev):
-        self.log.info(
-            "Checking out git worktree for revision {}".format(git_rev)
-        )
+    def get_clean_worktree_for_git_revision(self, remote_repo, git_rev):
+        """Make a separate worktree with git_rev checked out.
+        Do not modify the current worktree."""
         git_worktree_path = tempfile.mkdtemp()
+        if remote_repo:
+            self.log.info(
+                "Checking out git worktree for revision {} from {}".format(
+                    git_rev, remote_repo
+                )
+            )
+            fetch_process = subprocess.Popen(
+                [self.git_command, "fetch", remote_repo, git_rev],
+                cwd=self.repo_path,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT
+            )
+            fetch_output, _ = fetch_process.communicate()
+            self.log.info(fetch_output.decode("utf-8"))
+            if fetch_process.returncode != 0:
+                raise Exception("Fetching revision failed, aborting")
+            worktree_rev = "FETCH_HEAD"
+        else:
+            self.log.info(
+                "Checking out git worktree for revision {}".format(git_rev)
+            )
+            worktree_rev = git_rev
         worktree_process = subprocess.Popen(
-            [self.git_command, "worktree", "add", "--detach", git_worktree_path, git_rev],
+            [self.git_command, "worktree", "add", "--detach",
+             git_worktree_path, worktree_rev],
             cwd=self.repo_path,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT
@@ -88,6 +125,7 @@ class AbiChecker(object):
             raise Exception("git submodule update failed, aborting")
 
     def build_shared_libraries(self, git_worktree_path):
+        """Build the shared libraries in the specified worktree."""
         my_environment = os.environ.copy()
         my_environment["CFLAGS"] = "-g -Og"
         my_environment["SHARED"] = "1"
@@ -104,6 +142,9 @@ class AbiChecker(object):
             raise Exception("make failed, aborting")
 
     def get_abi_dumps_from_shared_libraries(self, git_ref, git_worktree_path):
+        """Generate the ABI dumps for the specified git revision.
+        It must be checked out in git_worktree_path and the shared libraries
+        must have been built."""
         abi_dumps = {}
         for mbed_module in self.mbedtls_modules:
             output_path = os.path.join(
@@ -129,6 +170,7 @@ class AbiChecker(object):
         return abi_dumps
 
     def cleanup_worktree(self, git_worktree_path):
+        """Remove the specified git worktree."""
         shutil.rmtree(git_worktree_path)
         worktree_process = subprocess.Popen(
             [self.git_command, "worktree", "prune"],
@@ -141,8 +183,11 @@ class AbiChecker(object):
         if worktree_process.returncode != 0:
             raise Exception("Worktree cleanup failed, aborting")
 
-    def get_abi_dump_for_ref(self, git_rev):
-        git_worktree_path = self.get_clean_worktree_for_git_revision(git_rev)
+    def get_abi_dump_for_ref(self, remote_repo, git_rev):
+        """Generate the ABI dumps for the specified git revision."""
+        git_worktree_path = self.get_clean_worktree_for_git_revision(
+            remote_repo, git_rev
+        )
         self.update_git_submodules(git_worktree_path)
         self.build_shared_libraries(git_worktree_path)
         abi_dumps = self.get_abi_dumps_from_shared_libraries(
@@ -152,6 +197,9 @@ class AbiChecker(object):
         return abi_dumps
 
     def get_abi_compatibility_report(self):
+        """Generate a report of the differences between the reference ABI
+        and the new ABI. ABI dumps from self.old_rev and self.new_rev must
+        be available."""
         compatibility_report = ""
         compliance_return_code = 0
         for mbed_module in self.mbedtls_modules:
@@ -201,10 +249,12 @@ class AbiChecker(object):
         return compliance_return_code
 
     def check_for_abi_changes(self):
+        """Generate a report of ABI differences
+        between self.old_rev and self.new_rev."""
         self.check_repo_path()
         self.check_abi_tools_are_installed()
-        self.old_dumps = self.get_abi_dump_for_ref(self.old_rev)
-        self.new_dumps = self.get_abi_dump_for_ref(self.new_rev)
+        self.old_dumps = self.get_abi_dump_for_ref(self.old_repo, self.old_rev)
+        self.new_dumps = self.get_abi_dump_for_ref(self.new_repo, self.new_rev)
         return self.get_abi_compatibility_report()
 
 
@@ -231,21 +281,43 @@ def run_main():
             help="keep all reports, even if there are no compatibility issues",
         )
         parser.add_argument(
-            "-o", "--old-rev", type=str, help="revision for old version",
-            required=True
+            "-o", "--old-rev", type=str,
+            help=("revision for old version."
+                  "Can include repository before revision"),
+            required=True, nargs="+"
         )
         parser.add_argument(
-            "-n", "--new-rev", type=str, help="revision for new version",
-            required=True
+            "-n", "--new-rev", type=str,
+            help=("revision for new version"
+                  "Can include repository before revision"),
+            required=True, nargs="+"
         )
         abi_args = parser.parse_args()
+        if len(abi_args.old_rev) == 1:
+            old_repo = None
+            old_rev = abi_args.old_rev[0]
+        elif len(abi_args.old_rev) == 2:
+            old_repo = abi_args.old_rev[0]
+            old_rev = abi_args.old_rev[1]
+        else:
+            raise Exception("Too many arguments passed for old version")
+        if len(abi_args.new_rev) == 1:
+            new_repo = None
+            new_rev = abi_args.new_rev[0]
+        elif len(abi_args.new_rev) == 2:
+            new_repo = abi_args.new_rev[0]
+            new_rev = abi_args.new_rev[1]
+        else:
+            raise Exception("Too many arguments passed for new version")
         abi_check = AbiChecker(
-            abi_args.report_dir, abi_args.old_rev,
-            abi_args.new_rev, abi_args.keep_all_reports
+            abi_args.report_dir, old_repo, old_rev,
+            new_repo, new_rev, abi_args.keep_all_reports
         )
         return_code = abi_check.check_for_abi_changes()
         sys.exit(return_code)
-    except Exception:
+    except Exception: # pylint: disable=broad-except
+        # Print the backtrace and exit explicitly so as to exit with
+        # status 2, not 1.
         traceback.print_exc()
         sys.exit(2)
 
