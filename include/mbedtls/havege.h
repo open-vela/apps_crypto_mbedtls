6e614494c5c1216d51805b1ed43dcd400c5578ef
liujinye <liujinye@xiaomi.com>
2019-06-18 14:21:23 +0800

    Merge remote-tracking branch 'origin/pr/2686' into mbedtls-2.18
    * origin/pr/2686:
      Update library version to 2.18.0

--- include/mbedtls/havege.h
+++ include/mbedtls/havege.h
@@ -31,7 +31,6 @@
 #endif
 
 #include <stddef.h>
-#include <stdint.h>
 
 #define MBEDTLS_HAVEGE_COLLECT_SIZE 1024
 
@@ -44,9 +43,9 @@ extern "C" {
  */
 typedef struct mbedtls_havege_state
 {
-    uint32_t PT1, PT2, offset[2];
-    uint32_t pool[MBEDTLS_HAVEGE_COLLECT_SIZE];
-    uint32_t WALK[8192];
+    int PT1, PT2, offset[2];
+    int pool[MBEDTLS_HAVEGE_COLLECT_SIZE];
+    int WALK[8192];
 }
 mbedtls_havege_state;
 
