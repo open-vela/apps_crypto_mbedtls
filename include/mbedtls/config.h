8393f9a7c6d845d324ac97984c4c23db4a43793c
liujinye <liujinye@xiaomi.com>
2019-07-29 11:48:23 +0800

    Merge pull request #273 from k-stachowiak/IOTCRYPT-726-unchecked-return-ecjpake
    Fix unchecked return value in the ECJPake module

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -1710,22 +1710,6 @@
 #define MBEDTLS_PSA_CRYPTO_C
 
 /**
- * \def MBEDTLS_PSA_CRYPTO_SE_C
- *
- * Enable secure element support in the Platform Security Architecture
- * cryptography API.
- *
- * \warning This feature is not yet suitable for production. It is provided
- *          for API evaluation and testing purposes only.
- *
- * Module:  library/psa_crypto_se.c
- *
- * Requires: MBEDTLS_PSA_CRYPTO_C, MBEDTLS_PSA_CRYPTO_STORAGE_C
- *
- */
-//#define MBEDTLS_PSA_CRYPTO_SE_C
-
-/**
  * \def MBEDTLS_PSA_CRYPTO_STORAGE_C
  *
  * Enable the Platform Security Architecture persistent key storage.
@@ -2016,6 +2000,6 @@
 #include MBEDTLS_USER_CONFIG_FILE
 #endif
 
-#include "check_config.h"
+#include "mbedtls/check_config.h"
 
 #endif /* MBEDTLS_CONFIG_H */
