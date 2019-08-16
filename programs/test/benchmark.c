3a662f0ca5f9d8f39bcfb87ffb6d689c941af571
liujinye <liujinye@xiaomi.com>
2019-08-16 09:26:41 +0800

    Merge pull request #196 from RonEld/fix_license_header
    Fix the license header of hkdf

--- programs/test/benchmark.c
+++ programs/test/benchmark.c
@@ -43,7 +43,7 @@
 int main( void )
 {
     mbedtls_printf("MBEDTLS_TIMING_C not defined.\n");
-    mbedtls_exit( 0 );
+    return( 0 );
 }
 #else
 
@@ -999,7 +999,7 @@ int main( int argc, char *argv[] )
     fflush( stdout ); getchar();
 #endif
 
-    mbedtls_exit( 0 );
+    return( 0 );
 }
 
 #endif /* MBEDTLS_TIMING_C */
