5220c3eab143e9e4f8e4d3a41f4df5e63259c38b
openvela-robot <openvela-robot@xiaomi.com>
2019-09-04 10:18:11 +0800

    Merge pull request #166 from k-stachowiak/IOTCRYPT-440-add-missing-dec_empty_buf-tests
    Add missing dec_empty_buf tests

--- include/psa/crypto_platform.h
+++ include/psa/crypto_platform.h
@@ -1,7 +1,7 @@
 /**
  * \file psa/crypto_platform.h
  *
- * \brief PSA cryptography module: Mbed TLS platform definitions
+ * \brief PSA cryptography module: Mbed TLS platfom definitions
  *
  * \note This file may not be included directly. Applications must
  * include psa/crypto.h.
