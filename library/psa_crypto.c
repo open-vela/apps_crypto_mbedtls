bd2d08f93e74073d58ae29680c0bd5a01caff736
liujinye <liujinye@xiaomi.com>
2019-10-31 17:03:28 +0800

    Merge pull request #288 from gilles-peskine-arm/psa-ecdsa_longer_hash
    Add ECDSA tests with hash and key of different lengths

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -1013,6 +1013,9 @@ psa_status_t psa_destroy_key( psa_key_handle_t handle )
     psa_se_drv_table_entry_t *driver;
 #endif /* MBEDTLS_PSA_CRYPTO_SE_C */
 
+    if( handle == 0 )
+        return( PSA_SUCCESS );
+
     status = psa_get_key_slot( handle, &slot );
     if( status != PSA_SUCCESS )
         return( status );
@@ -2003,7 +2006,6 @@ exit:
 /* Message digests */
 /****************************************************************/
 
-#if defined(MBEDTLS_RSA_C) || defined(MBEDTLS_ECDSA_DETERMINISTIC)
 static const mbedtls_md_info_t *mbedtls_md_info_from_psa( psa_algorithm_t alg )
 {
     switch( alg )
@@ -2044,7 +2046,6 @@ static const mbedtls_md_info_t *mbedtls_md_info_from_psa( psa_algorithm_t alg )
             return( NULL );
     }
 }
-#endif
 
 psa_status_t psa_hash_abort( psa_hash_operation_t *operation )
 {
