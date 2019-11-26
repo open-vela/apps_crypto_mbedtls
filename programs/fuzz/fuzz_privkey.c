3c707c48a095be809a71f03307aa6fb38991bff0
openvela-robot <openvela-robot@xiaomi.com>
2019-11-26 16:32:40 +0800

    Add an acceptance test for memory usage after handshake Signed-off-by: Piotr Nowicki <piotr.nowicki@arm.com> Signed-off-by: Andrzej Kurek <andrzej.kurek@arm.com>

--- programs/fuzz/fuzz_privkey.c
+++ programs/fuzz/fuzz_privkey.c
@@ -44,26 +44,20 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
         else
 #endif
 #if defined(MBEDTLS_ECP_C)
-        if( mbedtls_pk_get_type( &pk ) == MBEDTLS_PK_ECKEY ||
-            mbedtls_pk_get_type( &pk ) == MBEDTLS_PK_ECKEY_DH )
+        if( mbedtls_pk_get_type( &pk ) == MBEDTLS_PK_ECKEY )
         {
-            mbedtls_ecp_keypair *ecp = mbedtls_pk_ec( pk );
-            mbedtls_ecp_group_id grp_id = ecp->grp.id;
-            const mbedtls_ecp_curve_info *curve_info =
-                mbedtls_ecp_curve_info_from_grp_id( grp_id );
+            mbedtls_ecp_keypair *ecp;
 
-            /* If the curve is not supported, the key should not have been
-             * accepted. */
-            if( curve_info == NULL )
-                abort( );
+            ecp = mbedtls_pk_ec( pk );
+            if (ecp) {
+                ret = 0;
+            }
         }
         else
 #endif
-        {
-            /* The key is valid but is not of a supported type.
-             * This should not happen. */
-            abort( );
-        }
+            {
+                ret = 0;
+            }
     }
     mbedtls_pk_free( &pk );
 #else
