d2e5979b49586cc80f55ff7d1915634b1e8cd869
liujinye <liujinye@xiaomi.com>
2020-02-26 18:26:46 +0800

    Revert "scripts: Remove dependency on NET"
    This reverts commit 356acc82ad413dfec8d49745793e94a2e2f4c69e.
    
    Conflicts:
    * scripts/generate_errors.pl: a line adjacent to a changed line has
      independently changed in the meantime. Just revert the change done
      in the commit that's being reverted.

--- scripts/generate_errors.pl
+++ scripts/generate_errors.pl
@@ -3,17 +3,15 @@
 # Generate error.c
 #
 # Usage: ./generate_errors.pl or scripts/generate_errors.pl without arguments,
-# or generate_errors.pl include_dir data_dir error_file include_crypto
-# include_crypto can be either 0 (don't include) or 1 (include). On by default.
+# or generate_errors.pl include_dir data_dir error_file
 
 use strict;
 
-my ($include_dir, $data_dir, $error_file, $include_crypto);
-my $crypto_dir = "crypto";
+my ($include_dir, $data_dir, $error_file);
 
 if( @ARGV ) {
-    die "Invalid number of arguments" if scalar @ARGV != 4;
-    ($include_dir, $data_dir, $error_file, $include_crypto) = @ARGV;
+    die "Invalid number of arguments" if scalar @ARGV != 3;
+    ($include_dir, $data_dir, $error_file) = @ARGV;
 
     -d $include_dir or die "No such directory: $include_dir\n";
     -d $data_dir or die "No such directory: $data_dir\n";
@@ -21,17 +19,12 @@ if( @ARGV ) {
     $include_dir = 'include/mbedtls';
     $data_dir = 'scripts/data_files';
     $error_file = 'library/error.c';
-    $include_crypto = 1;
 
     unless( -d $include_dir && -d $data_dir ) {
         chdir '..' or die;
         -d $include_dir && -d $data_dir
             or die "Without arguments, must be run from root or scripts\n"
     }
-}
-
-if( $include_crypto ) {
-    -d $crypto_dir or die "Crypto submodule not present\n";
 }
 
 my $error_format_file = $data_dir.'/error.fmt';
@@ -43,7 +36,7 @@ my @low_level_modules = qw( AES ARC4 ARIA ASN1 BASE64 BIGNUM BLOWFISH
                             SHA1 SHA256 SHA512 THREADING XTEA );
 my @high_level_modules = qw( CIPHER DHM ECP MD
                              PEM PK PKCS12 PKCS5
-                             RSA SSL X509 );
+                             RSA );
 
 my $line_separator = $/;
 undef $/;
@@ -54,19 +47,9 @@ close(FORMAT_FILE);
 
 $/ = $line_separator;
 
-my @headers = ();
-if ($include_crypto) {
-    @headers = <$crypto_dir/$include_dir/*.h>;
-    foreach my $header (<$include_dir/*.h>) {
-        my $basename = $header; $basename =~ s!.*/!!;
-        push @headers, $header unless -e "$crypto_dir/$include_dir/$basename";
-    }
-} else {
-     @headers = <$include_dir/*.h>;
-}
-
+my @files = <$include_dir/*.h>;
 my @matches;
-foreach my $file (@headers) {
+foreach my $file (@files) {
     open(FILE, "$file");
     my @grep_res = grep(/^\s*#define\s+MBEDTLS_ERR_\w+\s+\-0x[0-9A-Fa-f]+/, <FILE>);
     push(@matches, @grep_res);
@@ -90,9 +73,8 @@ foreach my $line (@matches)
     my ($error_name, $error_code) = $line =~ /(MBEDTLS_ERR_\w+)\s+\-(0x\w+)/;
     my ($description) = $line =~ /\/\*\*< (.*?)\.? \*\//;
 
-    if( $error_codes_seen{$error_code}++ ) {
-        die "Duplicated error code: $error_code ($error_name)\n";
-    }
+    die "Duplicated error code: $error_code ($error_name)\n"
+        if( $error_codes_seen{$error_code}++ );
 
     $description =~ s/\\/\\\\/g;
     if ($description eq "") {
@@ -108,9 +90,7 @@ foreach my $line (@matches)
     $module_name = "HMAC_DRBG" if ($module_name eq "HMAC");
 
     my $define_name = $module_name;
-    $define_name = "X509_USE,X509_CREATE" if ($define_name eq "X509");
     $define_name = "ASN1_PARSE" if ($define_name eq "ASN1");
-    $define_name = "SSL_TLS" if ($define_name eq "SSL");
     $define_name = "PEM_PARSE,PEM_WRITE" if ($define_name eq "PEM");
 
     my $include_name = $module_name;
@@ -178,19 +158,8 @@ foreach my $line (@matches)
         ${$old_define} = $define_name;
     }
 
-    if ($error_name eq "MBEDTLS_ERR_SSL_FATAL_ALERT_MESSAGE")
-    {
-        ${$code_check} .= "${white_space}if( use_ret == -($error_name) )\n".
-                          "${white_space}\{\n".
-                          "${white_space}    mbedtls_snprintf( buf, buflen, \"$module_name - $description\" );\n".
-                          "${white_space}    return;\n".
-                          "${white_space}}\n"
-    }
-    else
-    {
-        ${$code_check} .= "${white_space}if( use_ret == -($error_name) )\n".
-                          "${white_space}    mbedtls_snprintf( buf, buflen, \"$module_name - $description\" );\n"
-    }
+    ${$code_check} .= "${white_space}if( use_ret == -($error_name) )\n".
+                      "${white_space}    mbedtls_snprintf( buf, buflen, \"$module_name - $description\" );\n"
 };
 
 if ($ll_old_define ne "")
