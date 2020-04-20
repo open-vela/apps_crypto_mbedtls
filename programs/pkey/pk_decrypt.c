ed9781e06c551167e604d7be9351cf6a4ba730ad
liujinye <liujinye@xiaomi.com>
2020-04-20 15:50:12 +0800

    Merge pull request #3194 from piotr-now/serialized_to_file
    Saving the serialized context to a file

--- programs/pkey/pk_decrypt.c
+++ programs/pkey/pk_decrypt.c
@@ -64,8 +64,7 @@ int main( void )
 int main( int argc, char *argv[] )
 {
     FILE *f;
-    int ret = 1;
-    unsigned c;
+    int ret = 1, c;
     int exit_code = MBEDTLS_EXIT_FAILURE;
     size_t i, olen = 0;
     mbedtls_pk_context pk;
