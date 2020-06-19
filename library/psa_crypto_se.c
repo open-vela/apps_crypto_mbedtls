860c5351b330284089c8e6ff1c4ed0769eeb3312
openvela-robot <openvela-robot@xiaomi.com>
2020-06-19 11:59:49 +0800

    Add length check in ecp_drbg_seed()
    While this is a static function, so right now we know we don't need the check,
    things may change in the future, so better be on the safe side.
    
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- library/psa_crypto_se.c
+++ library/psa_crypto_se.c
@@ -1,8 +1,7 @@
 /*
  *  PSA crypto support for secure element drivers
  */
-/*
- *  Copyright (C) 2019, ARM Limited, All Rights Reserved
+/*  Copyright (C) 2019, ARM Limited, All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
