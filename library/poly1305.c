2665c02869630003f0c68a139dd1f32e66460029
openvela-robot <openvela-robot@xiaomi.com>
2020-06-22 17:02:34 +0800

    mbedtls: use built-in platform entropy functions.
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- library/poly1305.c
+++ library/poly1305.c
@@ -513,6 +513,7 @@ static const unsigned char test_mac[2][16] =
     }
 };
 
+#undef ASSERT
 #define ASSERT( cond, args )            \
     do                                  \
     {                                   \
