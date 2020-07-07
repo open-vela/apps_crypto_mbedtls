fe481d1a383e863e6f082ff71d06867b9bc175d4
openvela-robot <openvela-robot@xiaomi.com>
2020-07-07 10:43:37 +0800

    Generate good/bad padding programmatically
    Compared to the previous approach of having the bad padding provided as input
    to the testing function, this allows to test more kinds of incorrect data,
    with less test cases in the .data file and more important no manually-generated
    non-trivial data in the test case parameters, making it much easier to
    complete the testing matrix.
    
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- library/padlock.c
+++ library/padlock.c
@@ -25,7 +25,11 @@
  *  programming_guide.pdf
  */
 
-#include "common.h"
+#if !defined(MBEDTLS_CONFIG_FILE)
+#include "mbedtls/config.h"
+#else
+#include MBEDTLS_CONFIG_FILE
+#endif
 
 #if defined(MBEDTLS_PADLOCK_C)
 
