126157a2c8377b5234d6322340e99f9f42791454
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2020-07-16 10:19:32 +0200

    Fix added proxy command when IPv6 is used
    For explicit proxy commands (included with `-p "$P_PXY <args>` in the test
    case), it's the test's writer responsibility to handle IPv6; only fix the
    proxy command when we're auto-adding it.
    
    Change-Id: I6c09ace4a646e1f8f072d52a4f336cb4b7231b92
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- tests/scripts/basic-build-test.sh
+++ tests/scripts/basic-build-test.sh
@@ -2,22 +2,9 @@
 
 # basic-build-tests.sh
 #
+# This file is part of mbed TLS (https://tls.mbed.org)
+#
 # Copyright (c) 2016, ARM Limited, All Rights Reserved
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# This file is part of Mbed TLS (https://tls.mbed.org)
 #
 # Purpose
 #
@@ -55,14 +42,6 @@ fi
 : ${GNUTLS_SERV:="gnutls-serv"}
 : ${GNUTLS_LEGACY_CLI:="$GNUTLS_CLI"}
 : ${GNUTLS_LEGACY_SERV:="$GNUTLS_SERV"}
-
-# Used to make ssl-opt.sh deterministic.
-#
-# See also RELEASE_SEED in all.sh. Debugging is easier if both values are kept
-# in sync. If you change the value here because it breaks some tests, you'll
-# definitely want to change it in all.sh as well.
-: ${SEED:=1}
-export SEED
 
 # To avoid setting OpenSSL and GnuTLS for each call to compat.sh and ssl-opt.sh
 # we just export the variables they require
