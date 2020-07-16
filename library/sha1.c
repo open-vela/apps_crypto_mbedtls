5d02be3c076f59ce949e236a8fa29fb1ac9ab97c
openvela-robot <openvela-robot@xiaomi.com>
2020-07-16 09:48:54 +0800

    Clarify some comments
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- library/sha1.c
+++ library/sha1.c
@@ -24,7 +24,11 @@
  *  http://www.itl.nist.gov/fipspubs/fip180-1.htm
  */
 
-#include "common.h"
+#if !defined(MBEDTLS_CONFIG_FILE)
+#include "mbedtls/config.h"
+#else
+#include MBEDTLS_CONFIG_FILE
+#endif
 
 #if defined(MBEDTLS_SHA1_C)
 
