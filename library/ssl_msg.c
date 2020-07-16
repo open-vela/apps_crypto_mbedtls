7141614e5812ee738a1570133482b84e86a9fc05
gabor-mezei-arm <gabor.mezei@arm.com>
2020-07-16 10:19:18 +0200

    Add missing newline
    Change-Id: I5f32bc04f48b35f9bf3bffa43816a225c09547b7
    Signed-off-by: gabor-mezei-arm <gabor.mezei@arm.com>

--- library/ssl_msg.c
+++ library/ssl_msg.c
@@ -28,11 +28,7 @@
  *  http://www.ietf.org/rfc/rfc4346.txt
  */
 
-#if !defined(MBEDTLS_CONFIG_FILE)
-#include "mbedtls/config.h"
-#else
-#include MBEDTLS_CONFIG_FILE
-#endif
+#include "common.h"
 
 #if defined(MBEDTLS_SSL_TLS_C)
 
@@ -5584,6 +5580,10 @@ int mbedtls_ssl_read( mbedtls_ssl_context *ssl, unsigned char *buf, size_t len )
 
     memcpy( buf, ssl->in_offt, n );
     ssl->in_msglen -= n;
+
+    /* Zeroising the plaintext buffer to erase unused application data
+       from the memory. */
+    mbedtls_platform_zeroize( ssl->in_offt, n );
 
     if( ssl->in_msglen == 0 )
     {
