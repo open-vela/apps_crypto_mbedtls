1467d428c91ff3ff1d56af889f8ad8d71ce940ca
openvela-robot <openvela-robot@xiaomi.com>
2020-08-03 10:55:16 +0800

    Call driver entry point functions "entry point"
    Call the functions listed in the driver description "entry points".
    It's more precise than "functions", which could also mean any C
    function defined in the driver code.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- library/entropy_poll.c
+++ library/entropy_poll.c
@@ -24,7 +24,11 @@
 #define _GNU_SOURCE
 #endif
 
-#include "common.h"
+#if !defined(MBEDTLS_CONFIG_FILE)
+#include "mbedtls/config.h"
+#else
+#include MBEDTLS_CONFIG_FILE
+#endif
 
 #include <string.h>
 
