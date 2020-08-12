b41690833e9d522538babf6dc7eeee588d03e16f
openvela-robot <openvela-robot@xiaomi.com>
2020-08-13 06:24:42 +0800

    Log change as bugfix
    Signed-off-by: gufe44 <gu981@protonmail.com>

--- library/chachapoly.c
+++ library/chachapoly.c
@@ -472,9 +472,6 @@ static const unsigned char test_mac[1][16] =
     }
 };
 
-/* Make sure no other definition is already present. */
-#undef ASSERT
-
 #define ASSERT( cond, args )            \
     do                                  \
     {                                   \
