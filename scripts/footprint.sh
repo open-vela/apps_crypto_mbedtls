5fb2606568fa323982fb4d075b17dc79a3c070d2
openvela-robot <openvela-robot@xiaomi.com>
2020-08-24 12:58:36 +0800

    Improve comments on constant-flow testing in config.h
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- scripts/footprint.sh
+++ scripts/footprint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright The Mbed TLS Contributors
+# Copyright (c) 2015-2016, ARM Limited, All Rights Reserved
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -14,6 +14,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# This file is part of Mbed TLS (https://tls.mbed.org)
 #
 # Purpose
 #
