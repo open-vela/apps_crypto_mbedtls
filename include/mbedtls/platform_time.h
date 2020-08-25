5e38ed643a138fea0424790420612b5953ff62bf
John Durkop <john.durkop@fermatsoftware.com>
2020-08-24 18:29:15 -0700

    Fix test issues with depends-hashes
    Needed to make additional fixes so that when MBEDTLS_USE_PSA_CRYPTO
    is defined, the depends-hashes test will succeed. There are two
    versions of the ecdsa_verify_wrap() function, one with
    MBEDTLS_USE_PSA_CRYPTO and when when it is not enabled. The non PSA
    version is not using the md_alg parameter since it is not required.
    The PSA version was using that parameter to derive a different value
    it needed for PSA_ALG_ECDSA. The arguement of PSA_ALG_ECDSA is
    ignored for psa_sign_hash and psa_verify_hash. It is present because
    it is used and must be a valid hash, not zero, for psa_sign_hash
    (but not psa_verify_hash) with PSA_ALG_DETERMINISTIC_ECDSA, and it is
    needed for psa_sign_message and psa_verify_message which are not
    implemented yet. The local parameter now uses PSA_ALG_ECDSA_ANY for
    the verify function to avoid using the md_alg parameter and avoids
    returning incorrect error codes.
    
    Fixes #3587
    
    Change-Id: I9c0c769c0168129e4b1541594a7fe8f711dec9a4
    Signed-off-by: John Durkop <john.durkop@fermatsoftware.com>

--- include/mbedtls/platform_time.h
+++ include/mbedtls/platform_time.h
@@ -4,7 +4,7 @@
  * \brief mbed TLS Platform time abstraction
  */
 /*
- *  Copyright The Mbed TLS Contributors
+ *  Copyright (C) 2006-2016, ARM Limited, All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -18,6 +18,8 @@
  *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *
+ *  This file is part of mbed TLS (https://tls.mbed.org)
  */
 #ifndef MBEDTLS_PLATFORM_TIME_H
 #define MBEDTLS_PLATFORM_TIME_H
