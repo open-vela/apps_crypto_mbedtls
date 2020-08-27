e5109c04b445d63ca5b4f8f94e1dc1bcb65d12ee
liujinye <liujinye@xiaomi.com>
2020-08-27 11:31:49 +0800

    Merge pull request #748 from ARMmbed/mbedtls-2.24.0r0-pr
    Prepare Release Candidate for Mbed TLS 2.24.0

--- library/entropy.c
+++ library/entropy.c
@@ -466,27 +466,28 @@ int mbedtls_entropy_update_nv_seed( mbedtls_entropy_context *ctx )
 #if defined(MBEDTLS_FS_IO)
 int mbedtls_entropy_write_seed_file( mbedtls_entropy_context *ctx, const char *path )
 {
-    int ret;
-    FILE *f = NULL;
+    int ret = MBEDTLS_ERR_ENTROPY_FILE_IO_ERROR;
+    FILE *f;
     unsigned char buf[MBEDTLS_ENTROPY_BLOCK_SIZE];
+
+    if( ( f = fopen( path, "wb" ) ) == NULL )
+        return( MBEDTLS_ERR_ENTROPY_FILE_IO_ERROR );
 
     if( ( ret = mbedtls_entropy_func( ctx, buf, MBEDTLS_ENTROPY_BLOCK_SIZE ) ) != 0 )
         goto exit;
 
-    ret = MBEDTLS_ERR_ENTROPY_FILE_IO_ERROR;
-    if( ( f = fopen( path, "wb" ) ) != NULL )
+    if( fwrite( buf, 1, MBEDTLS_ENTROPY_BLOCK_SIZE, f ) != MBEDTLS_ENTROPY_BLOCK_SIZE )
     {
-        if( fwrite( buf, 1, MBEDTLS_ENTROPY_BLOCK_SIZE, f ) != MBEDTLS_ENTROPY_BLOCK_SIZE )
-            goto exit;
-        ret = 0;
+        ret = MBEDTLS_ERR_ENTROPY_FILE_IO_ERROR;
+        goto exit;
     }
+
+    ret = 0;
 
 exit:
     mbedtls_platform_zeroize( buf, sizeof( buf ) );
 
-    if( f )
-        fclose( f );
-
+    fclose( f );
     return( ret );
 }
 
