657366ffc3129ef11f77072ccfee3c3902a22978
openvela-robot <openvela-robot@xiaomi.com>
2020-09-07 17:14:14 +0800

    Fix style and pull out ECB processing in separate function
    Signed-off-by: Steven Cooreman <steven.cooreman@silabs.com>

--- include/psa/crypto_driver_common.h
+++ include/psa/crypto_driver_common.h
@@ -17,7 +17,7 @@
  */
 
 /*
- *  Copyright The Mbed TLS Contributors
+ *  Copyright (C) 2018, ARM Limited, All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
