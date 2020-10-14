545489ac6c34ac7e5fbdbf396e13f5a08e22d78d
John Durkop <john.durkop@fermatsoftware.com>
2020-10-13 19:58:20 -0700

    Correct use of MBEDTLS_PSA_BUILTIN_ALG_xxx in crypto library
    The psa crypto library was generically using PSA_WANT_ALG_xxx, but
    should have been using the correct MBEDTLS_PSA_BUILTIN_ALG_xxx
    definition since that code is the builtin version. There were also
    a couple of spots that needed to ensure the code block was enabled
    for either ECDSA or DETERMINISTIC_ECDSA, not just one of them.
    
    Fixed all the new ALG_ECDSA_DETERMINISTIC names to be
    ALG_DETERMINISTIC_ECDSA instead.
    
    Fixed test to use correct definitions.
    
    Change-Id: I0864cfd11f1d2a6fbac931e2a1688bcd69669c37
    Signed-off-by: John Durkop <john.durkop@fermatsoftware.com>

--- programs/test/query_config.c
+++ programs/test/query_config.c
@@ -1168,13 +1168,13 @@ int query_config( const char *config )
     }
 #endif /* MBEDTLS_ENTROPY_NV_SEED */
 
-#if defined(MBEDTLS_PSA_CRYPTO_KEY_FILE_ID_ENCODES_OWNER)
-    if( strcmp( "MBEDTLS_PSA_CRYPTO_KEY_FILE_ID_ENCODES_OWNER", config ) == 0 )
+#if defined(MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER)
+    if( strcmp( "MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER", config ) == 0 )
     {
-        MACRO_EXPANSION_TO_STR( MBEDTLS_PSA_CRYPTO_KEY_FILE_ID_ENCODES_OWNER );
+        MACRO_EXPANSION_TO_STR( MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER );
         return( 0 );
     }
-#endif /* MBEDTLS_PSA_CRYPTO_KEY_FILE_ID_ENCODES_OWNER */
+#endif /* MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER */
 
 #if defined(MBEDTLS_MEMORY_DEBUG)
     if( strcmp( "MBEDTLS_MEMORY_DEBUG", config ) == 0 )
@@ -1591,6 +1591,14 @@ int query_config( const char *config )
         return( 0 );
     }
 #endif /* MBEDTLS_USE_PSA_CRYPTO */
+
+#if defined(MBEDTLS_PSA_CRYPTO_CONFIG)
+    if( strcmp( "MBEDTLS_PSA_CRYPTO_CONFIG", config ) == 0 )
+    {
+        MACRO_EXPANSION_TO_STR( MBEDTLS_PSA_CRYPTO_CONFIG );
+        return( 0 );
+    }
+#endif /* MBEDTLS_PSA_CRYPTO_CONFIG */
 
 #if defined(MBEDTLS_VERSION_FEATURES)
     if( strcmp( "MBEDTLS_VERSION_FEATURES", config ) == 0 )
