f427b09bcc4276fce1f562e21226e60bbfaf5c42
Steven Cooreman <steven.cooreman@silabs.com>
2020-10-14 14:39:20 +0200

    Plug in the entry point for public key export through driver
    Including test.
    
    Signed-off-by: Steven Cooreman <steven.cooreman@silabs.com>

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -1681,13 +1681,23 @@ static psa_status_t psa_internal_export_key( const psa_key_slot_t *slot,
             /* Exporting private -> private */
             return( psa_internal_export_key_buffer( slot, data, data_size, data_length ) );
         }
+
         /* Need to export the public part of a private key,
-         * so conversion is needed */
+         * so conversion is needed. Try the accelerators first. */
+        psa_status_t status = psa_driver_wrapper_export_public_key( slot,
+                                                                    data,
+                                                                    data_size,
+                                                                    data_length );
+
+        if( status != PSA_ERROR_NOT_SUPPORTED ||
+            psa_key_lifetime_is_external( slot->attr.lifetime ) )
+            return( status );
+
         if( PSA_KEY_TYPE_IS_RSA( slot->attr.type ) )
         {
 #if defined(MBEDTLS_RSA_C)
             mbedtls_rsa_context *rsa = NULL;
-            psa_status_t status = psa_load_rsa_representation(
+            status = psa_load_rsa_representation(
                                     slot->attr.type,
                                     slot->data.key.data,
                                     slot->data.key.bytes,
@@ -1714,7 +1724,7 @@ static psa_status_t psa_internal_export_key( const psa_key_slot_t *slot,
         {
 #if defined(MBEDTLS_ECP_C)
             mbedtls_ecp_keypair *ecp = NULL;
-            psa_status_t status = psa_load_ecp_representation(
+            status = psa_load_ecp_representation(
                                     slot->attr.type,
                                     slot->data.key.data,
                                     slot->data.key.bytes,
