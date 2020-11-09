93e69f9a2b99055254ccc2907ca40882eed1fb77
openvela-robot <openvela-robot@xiaomi.com>
2020-11-09 15:32:18 +0800

    Use ARCH_CHIP_AMEBAZ config instead since custom board/chip used
    N/A
    
    Signed-off-by: liuhaitao <liuhaitao@xiaomi.com>

--- include/mbedtls/poly1305.h
+++ include/mbedtls/poly1305.h
@@ -12,8 +12,7 @@
  * \author Daniel King <damaki.gh@gmail.com>
  */
 
-/*
- *  Copyright The Mbed TLS Contributors
+/*  Copyright (C) 2006-2018, Arm Limited (or its affiliates), All Rights Reserved.
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -27,6 +26,8 @@
  *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *
+ *  This file is part of Mbed TLS (https://tls.mbed.org)
  */
 
 #ifndef MBEDTLS_POLY1305_H
