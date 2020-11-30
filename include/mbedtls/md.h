5cb7b4eb5c5a8ed3fc4557a7d787bdfbfc2d72cf
chao.an <anchao@xiaomi.com>
2020-11-30 19:55:58 +0800

    mbedtls: remove vendor private part
    Change-Id: Ief6edfe52d2381abe3329f53d47ecdb8bda62405

--- include/mbedtls/md.h
+++ include/mbedtls/md.h
@@ -6,7 +6,7 @@
  * \author Adriaan de Jong <dejong@fox-it.com>
  */
 /*
- *  Copyright The Mbed TLS Contributors
+ *  Copyright (C) 2006-2018, Arm Limited (or its affiliates), All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -20,6 +20,8 @@
  *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *
+ *  This file is part of Mbed TLS (https://tls.mbed.org)
  */
 
 #ifndef MBEDTLS_MD_H
@@ -101,8 +103,6 @@ typedef struct mbedtls_md_context_t
 /**
  * \brief           This function returns the list of digests supported by the
  *                  generic digest module.
- *
- * \note            The list starts with the strongest available hashes.
  *
  * \return          A statically allocated array of digests. Each element
  *                  in the returned list is an integer belonging to the
