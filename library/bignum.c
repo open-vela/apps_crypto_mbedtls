ac78930226c0540a4b22d331208ae0b82b4628d7
openvela-robot <openvela-robot@xiaomi.com>
2020-12-07 13:06:47 +0800

    Merge pull request #3942 from gilles-peskine-arm/ecp-bignum-error-checks-development
    Add missing some error checks in ECP and bignum

--- library/bignum.c
+++ library/bignum.c
@@ -1411,7 +1411,10 @@ int mbedtls_mpi_sub_abs( mbedtls_mpi *X, const mbedtls_mpi *A, const mbedtls_mpi
         /* If we ran out of space for the carry, it means that the result
          * is negative. */
         if( n == X->n )
-            return( MBEDTLS_ERR_MPI_NEGATIVE_VALUE );
+        {
+            ret = MBEDTLS_ERR_MPI_NEGATIVE_VALUE;
+            goto cleanup;
+        }
         --X->p[n];
     }
 
@@ -2391,7 +2394,7 @@ int mbedtls_mpi_fill_random( mbedtls_mpi *X, size_t size,
     MBEDTLS_MPI_CHK( mbedtls_mpi_lset( X, 0 ) );
 
     Xp = (unsigned char*) X->p;
-    MBEDTLS_MPI_CHK( f_rng( p_rng, Xp + overhead, size ) );
+    f_rng( p_rng, Xp + overhead, size );
 
     mpi_bigendian_to_host( X->p, limbs );
 
