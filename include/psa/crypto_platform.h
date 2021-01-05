9055637adf0f15006fa2c4be7c3a62a0a7900705
liujinye <liujinye@xiaomi.com>
2021-01-05 11:49:18 +0800

    Clarify interoperability non-requirement
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- include/psa/crypto_platform.h
+++ include/psa/crypto_platform.h
@@ -41,10 +41,6 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-/* Translate between classic MBEDTLS_xxx feature symbols and PSA_xxx
- * feature symbols. */
-#include "mbedtls/config_psa.h"
-
 /* PSA requires several types which C99 provides in stdint.h. */
 #include <stdint.h>
 
@@ -52,6 +48,9 @@
     !defined(inline) && !defined(__cplusplus)
 #define inline __inline
 #endif
+
+/* Integral type representing a key handle. */
+typedef uint16_t psa_key_handle_t;
 
 #if defined(MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER)
 
