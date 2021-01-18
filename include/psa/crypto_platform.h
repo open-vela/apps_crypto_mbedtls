4e70bb60cdb859ba9497df1768871f6ad0adcfd4
openvela-robot <openvela-robot@xiaomi.com>
2021-01-18 23:36:18 +0800

    Clarify the methods of key storage testing
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- include/psa/crypto_platform.h
+++ include/psa/crypto_platform.h
@@ -41,10 +41,6 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-/* Translate between classic MBEDTLS_xxx feature symbols and PSA_xxx
- * feature symbols. */
-#include "mbedtls/config_psa.h"
-
 /* PSA requires several types which C99 provides in stdint.h. */
 #include <stdint.h>
 
@@ -52,6 +48,9 @@
     !defined(inline) && !defined(__cplusplus)
 #define inline __inline
 #endif
+
+/* Integral type representing a key handle. */
+typedef uint16_t psa_key_handle_t;
 
 #if defined(MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER)
 
@@ -80,20 +79,5 @@ static inline int mbedtls_key_owner_id_equal( mbedtls_key_owner_id_t id1,
 }
 
 #endif /* MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER */
-
-#if defined(MBEDTLS_PSA_CRYPTO_EXTERNAL_RNG)
-/** The type of the context passed to mbedtls_psa_external_get_random().
- *
- * Mbed TLS initializes the context to all-bits-zero before calling
- * mbedtls_psa_external_get_random() for the first time.
- *
- * The definition of this type in the Mbed TLS source code is for
- * demonstration purposes. Implementers of mbedtls_psa_external_get_random()
- * are expected to replace it with a custom definition.
- */
-typedef struct {
-    uintptr_t opaque[2];
-} mbedtls_psa_external_random_context_t;
-#endif /* MBEDTLS_PSA_CRYPTO_EXTERNAL_RNG */
 
 #endif /* PSA_CRYPTO_PLATFORM_H */
