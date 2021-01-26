aae9968f5b516580a31bcffcd8c126daff33fd56
Steven Cooreman <steven.cooreman@silabs.com>
2021-01-26 12:01:22 +0100

    Apply review feedback
    Change-Id: I0a3cd1f52972760a22b91ecafe521d076221a3b6
    Signed-off-by: Steven Cooreman <steven.cooreman@silabs.com>

--- programs/test/query_compile_time_config.c
+++ programs/test/query_compile_time_config.c
@@ -40,7 +40,7 @@
     "Mbed TLS build and the macro expansion of that configuration will be\n" \
     "printed (if any). Otherwise, 1 will be returned.\n"
 
-#include "query_config.h"
+int query_config( const char *config );
 
 int main( int argc, char *argv[] )
 {
