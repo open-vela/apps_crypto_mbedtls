cb0d0e5962c7ba7fd33ffc9c14d9c5ed6bf2f614
Steven Cooreman <steven.cooreman@silabs.com>
2021-02-10 17:13:28 +0100

    Fix unused variable in PSA core when no AEAD algorithm is present
    Signed-off-by: Steven Cooreman <steven.cooreman@silabs.com>

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -5102,6 +5102,7 @@ psa_status_t psa_aead_encrypt( mbedtls_svc_key_id_t key,
     else
 #endif /* MBEDTLS_CHACHAPOLY_C */
     {
+        (void) tag;
         return( PSA_ERROR_NOT_SUPPORTED );
     }
 
