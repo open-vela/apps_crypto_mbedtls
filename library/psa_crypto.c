26aa210607394e419e4c54dbbaf6e117c84612e8
Steven Cooreman <steven.cooreman@silabs.com>
2021-02-10 17:19:22 +0100

    Fix unused variables in PSA core when no KDF algorithm is present
    Signed-off-by: Steven Cooreman <steven.cooreman@silabs.com>

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -5609,6 +5609,7 @@ psa_status_t psa_key_derivation_output_bytes(
 #endif /* MBEDTLS_PSA_BUILTIN_ALG_TLS12_PRF ||
         * MBEDTLS_PSA_BUILTIN_ALG_TLS12_PSK_TO_MS */
     {
+        (void) kdf_alg;
         return( PSA_ERROR_BAD_STATE );
     }
 
@@ -6077,6 +6078,9 @@ static psa_status_t psa_key_derivation_input_internal(
 #endif /* MBEDTLS_PSA_BUILTIN_ALG_TLS12_PSK_TO_MS */
     {
         /* This can't happen unless the operation object was not initialized */
+        (void) data;
+        (void) data_length;
+        (void) kdf_alg;
         return( PSA_ERROR_BAD_STATE );
     }
 
