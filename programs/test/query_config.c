23da48bdb03deae546715541591e0d8e6b1a7d0b
liujinye <liujinye@xiaomi.com>
2021-03-05 10:48:38 +0800

    Merge pull request #4150 from mstarzyk-mobica/remove_compat13
    Remove compat-1.3

--- programs/test/query_config.c
+++ programs/test/query_config.c
@@ -64,7 +64,6 @@
 #include "mbedtls/entropy_poll.h"
 #include "mbedtls/error.h"
 #include "mbedtls/gcm.h"
-#include "mbedtls/havege.h"
 #include "mbedtls/hkdf.h"
 #include "mbedtls/hmac_drbg.h"
 #include "mbedtls/md.h"
@@ -658,14 +657,6 @@ int query_config( const char *config )
     }
 #endif /* MBEDTLS_ECP_INTERNAL_ALT */
 
-#if defined(MBEDTLS_ECP_NO_FALLBACK)
-    if( strcmp( "MBEDTLS_ECP_NO_FALLBACK", config ) == 0 )
-    {
-        MACRO_EXPANSION_TO_STR( MBEDTLS_ECP_NO_FALLBACK );
-        return( 0 );
-    }
-#endif /* MBEDTLS_ECP_NO_FALLBACK */
-
 #if defined(MBEDTLS_ECP_RANDOMIZE_JAC_ALT)
     if( strcmp( "MBEDTLS_ECP_RANDOMIZE_JAC_ALT", config ) == 0 )
     {
@@ -1225,14 +1216,6 @@ int query_config( const char *config )
         return( 0 );
     }
 #endif /* MBEDTLS_PKCS1_V21 */
-
-#if defined(MBEDTLS_PSA_CRYPTO_CLIENT)
-    if( strcmp( "MBEDTLS_PSA_CRYPTO_CLIENT", config ) == 0 )
-    {
-        MACRO_EXPANSION_TO_STR( MBEDTLS_PSA_CRYPTO_CLIENT );
-        return( 0 );
-    }
-#endif /* MBEDTLS_PSA_CRYPTO_CLIENT */
 
 #if defined(MBEDTLS_PSA_CRYPTO_DRIVERS)
     if( strcmp( "MBEDTLS_PSA_CRYPTO_DRIVERS", config ) == 0 )
@@ -1913,14 +1896,6 @@ int query_config( const char *config )
         return( 0 );
     }
 #endif /* MBEDTLS_GCM_C */
-
-#if defined(MBEDTLS_HAVEGE_C)
-    if( strcmp( "MBEDTLS_HAVEGE_C", config ) == 0 )
-    {
-        MACRO_EXPANSION_TO_STR( MBEDTLS_HAVEGE_C );
-        return( 0 );
-    }
-#endif /* MBEDTLS_HAVEGE_C */
 
 #if defined(MBEDTLS_HKDF_C)
     if( strcmp( "MBEDTLS_HKDF_C", config ) == 0 )
@@ -2641,14 +2616,6 @@ int query_config( const char *config )
         return( 0 );
     }
 #endif /* MBEDTLS_PSA_HMAC_DRBG_MD_TYPE */
-
-#if defined(MBEDTLS_PSA_KEY_SLOT_COUNT)
-    if( strcmp( "MBEDTLS_PSA_KEY_SLOT_COUNT", config ) == 0 )
-    {
-        MACRO_EXPANSION_TO_STR( MBEDTLS_PSA_KEY_SLOT_COUNT );
-        return( 0 );
-    }
-#endif /* MBEDTLS_PSA_KEY_SLOT_COUNT */
 
 #if defined(MBEDTLS_SSL_CACHE_DEFAULT_TIMEOUT)
     if( strcmp( "MBEDTLS_SSL_CACHE_DEFAULT_TIMEOUT", config ) == 0 )
