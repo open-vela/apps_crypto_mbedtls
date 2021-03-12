73a54681bf5a8dbaf2957c72980d02f1d1804487
Ronald Cron <ronald.cron@arm.com>
2021-03-12 09:17:38 +0100

    Merge pull request #4220 from bensze01/psa_aead_update_output_size
    Fix revert of PSA_AEAD_UPDATE_OUTPUT_SIZE
    
    Change-Id: I0c1c4e89735773f0bf9d53643b2b9e0de6176c2d

--- programs/ssl/ssl_context_info.c
+++ programs/ssl/ssl_context_info.c
@@ -48,6 +48,7 @@ int main( void )
 #include "mbedtls/error.h"
 #include "mbedtls/base64.h"
 #include "mbedtls/md.h"
+#include "mbedtls/md_internal.h"
 #include "mbedtls/x509_crt.h"
 #include "mbedtls/ssl_ciphersuites.h"
 
@@ -637,7 +638,7 @@ void print_deserialized_ssl_session( const uint8_t *ssl, uint32_t len,
         }
         else
         {
-            printf( "\tMessage-Digest : %s\n", mbedtls_md_get_name( md_info ) );
+            printf( "\tMessage-Digest : %s\n", md_info->name );
         }
     }
 
