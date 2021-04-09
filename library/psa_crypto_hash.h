652e28d36ad422b3b38d7e7e38c7a3a643e12551
liujinye <liujinye@xiaomi.com>
2021-04-09 14:40:19 +0800

    Merge pull request #4311 from gilles-peskine-arm/move-internal-headers-doxygen
    [3.0] Remove obsolete reference to internal headers under include/

--- library/psa_crypto_hash.h
+++ library/psa_crypto_hash.h
@@ -22,8 +22,9 @@
 #define PSA_CRYPTO_HASH_H
 
 #include <psa/crypto.h>
+#include <psa/crypto_builtin_hash.h>
 
-#include <mbedtls/md_internal.h>
+#include "md_wrap.h"
 
 /** Get Mbed TLS MD information of a hash algorithm given its PSA identifier
  *
