d441d32d21511455d4eb21b4cb8b8e75a86fcae9
Mateusz Starzyk <mateusz.starzyk@mobica.com>
2021-04-19 16:46:28 +0200

    Separate SHA224 from SHA256 config options.
    These options are still dependant on each other.
    This is an intermediate step.
    
    Signed-off-by: Mateusz Starzyk <mateusz.starzyk@mobica.com>

--- library/version_features.c
+++ library/version_features.c
@@ -762,6 +762,9 @@ static const char * const features[] = {
 #if defined(MBEDTLS_SHA256_C)
     "MBEDTLS_SHA256_C",
 #endif /* MBEDTLS_SHA256_C */
+#if defined(MBEDTLS_SHA224_C)
+    "MBEDTLS_SHA224_C",
+#endif /* MBEDTLS_SHA224_C */
 #if defined(MBEDTLS_SHA512_C)
     "MBEDTLS_SHA512_C",
 #endif /* MBEDTLS_SHA512_C */
