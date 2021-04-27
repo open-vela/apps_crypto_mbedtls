0b05a4b4e23c1e71519d6102d1c4749426f69d23
Dave Rodgman <dave.rodgman@arm.com>
2021-04-27 17:01:24 +0100

    Improve changelog entry for #4217
    Change-Id: Ia03782831c17a3414de3c5e0af9e35bbf3e70e27
    Signed-off-by: Dave Rodgman <dave.rodgman@arm.com>

--- tests/scripts/check-names.sh
+++ tests/scripts/check-names.sh
@@ -28,6 +28,11 @@ EOF
     exit
 fi
 
+if grep --version|head -n1|grep GNU >/dev/null; then :; else
+    echo "This script requires GNU grep.">&2
+    exit 1
+fi
+
 trace=
 if [ $# -ne 0 ] && [ "$1" = "-v" ]; then
   shift
@@ -90,16 +95,15 @@ done
 
 printf "Likely typos: "
 sort -u actual-macros enum-consts > _caps
-HEADERS=$( ls include/mbedtls/*.h include/psa/*.h )
+HEADERS=$( ls include/mbedtls/*.h include/psa/*.h | egrep -v 'compat-1\.3\.h' )
 HEADERS="$HEADERS library/*.h"
 HEADERS="$HEADERS 3rdparty/everest/include/everest/everest.h 3rdparty/everest/include/everest/x25519.h"
 LIBRARY="$( ls library/*.c )"
 LIBRARY="$LIBRARY 3rdparty/everest/library/everest.c 3rdparty/everest/library/x25519.c"
 NL='
 '
-cat $HEADERS $LIBRARY \
-    | grep -v -e '//no-check-names' -e '#error' \
-    | sed -n 's/MBED..._[A-Z0-9_]*/\'"$NL"'&\'"$NL"/gp \
+sed -n 's/MBED..._[A-Z0-9_]*/\'"$NL"'&\'"$NL"/gp \
+    $HEADERS $LIBRARY \
     | grep MBEDTLS | sort -u > _MBEDTLS_XXX
 TYPOS=$( diff _caps _MBEDTLS_XXX | sed -n 's/^> //p' \
             | egrep -v 'XXX|__|_$|^MBEDTLS_.*CONFIG_FILE$' || true )
