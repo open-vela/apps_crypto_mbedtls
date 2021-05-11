7373924fa48ac49136b9861db04fcf8767912da3
liujinye <liujinye@xiaomi.com>
2021-05-11 11:09:13 +0800

    Be explicit about why the zero-length check is there
    Since a valid mac operation context would guarantee that the stored
    mac size is >= 4, it wasn't immediately obvious that the zero-length
    check is meant for static analyzers and a bit of robustness.
    
    Signed-off-by: Steven Cooreman <steven.cooreman@silabs.com>

--- include/mbedtls/net.h
+++ include/mbedtls/net.h
@@ -1,7 +1,12 @@
-/*
- * Umbrella include for all of the test driver functionality
+/**
+ * \file net.h
+ *
+ * \brief Deprecated header file that includes net_sockets.h
+ *
+ * \deprecated Superseded by mbedtls/net_sockets.h
  */
-/*  Copyright The Mbed TLS Contributors
+/*
+ *  Copyright The Mbed TLS Contributors
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -16,18 +21,15 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+#if !defined(MBEDTLS_CONFIG_FILE)
+#include "mbedtls/config.h"
+#else
+#include MBEDTLS_CONFIG_FILE
+#endif
 
-#ifndef PSA_CRYPTO_TEST_DRIVER_H
-#define PSA_CRYPTO_TEST_DRIVER_H
-
-#define PSA_CRYPTO_TEST_DRIVER_LOCATION 0x7fffff
-
-#include "test/drivers/aead.h"
-#include "test/drivers/cipher.h"
-#include "test/drivers/hash.h"
-#include "test/drivers/mac.h"
-#include "test/drivers/key_management.h"
-#include "test/drivers/signature.h"
-#include "test/drivers/size.h"
-
-#endif /* PSA_CRYPTO_TEST_DRIVER_H */
+#if !defined(MBEDTLS_DEPRECATED_REMOVED)
+#include "mbedtls/net_sockets.h"
+#if defined(MBEDTLS_DEPRECATED_WARNING)
+#warning "Deprecated header file: Superseded by mbedtls/net_sockets.h"
+#endif /* MBEDTLS_DEPRECATED_WARNING */
+#endif /* !MBEDTLS_DEPRECATED_REMOVED */
