233cd3cb13a755aa7bc688967fb5714e6ac96058
openvela-robot <openvela-robot@xiaomi.com>
2021-06-15 20:32:07 +0800

    Merge pull request #4629 from TRodziewicz/rename_functions_whose_deprecated_variants_have_been_removd
    Rename the _ret() functions

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -47,6 +47,7 @@
  *
  * Used in:
  *      library/aria.c
+ *      library/timing.c
  *      library/bn_mul.h
  *
  * Required by:
@@ -267,7 +268,7 @@
 /**
  * \def MBEDTLS_TIMING_ALT
  *
- * Uncomment to provide your own alternate implementation for
+ * Uncomment to provide your own alternate implementation for mbedtls_timing_hardclock(),
  * mbedtls_timing_get_timer(), mbedtls_set_alarm(), mbedtls_set/get_delay()
  *
  * Only works if you have MBEDTLS_TIMING_C enabled.
@@ -1014,7 +1015,7 @@
  * \def MBEDTLS_NO_DEFAULT_ENTROPY_SOURCES
  *
  * Do not add default entropy sources. These are the platform specific
- * poll function.
+ * or mbedtls_timing_hardclock poll function.
  *
  * This is useful to have more control over the added entropy sources in an
  * application.
