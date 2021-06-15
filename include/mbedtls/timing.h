ae3dbee209d467beeac2d89780304dbf9dd85f9a
openvela-robot <openvela-robot@xiaomi.com>
2021-06-15 20:32:07 +0800

    Merge pull request #4629 from TRodziewicz/rename_functions_whose_deprecated_variants_have_been_removd
    Rename the _ret() functions

--- include/mbedtls/timing.h
+++ include/mbedtls/timing.h
@@ -64,9 +64,19 @@ typedef struct mbedtls_timing_delay_context
 extern volatile int mbedtls_timing_alarmed;
 
 /**
- * \brief          Return the elapsed time in milliseconds
+ * \brief          Return the CPU cycle counter value
  *
- * \warning        May change without notice
+ * \warning        This is only a best effort! Do not rely on this!
+ *                 In particular, it is known to be unreliable on virtual
+ *                 machines.
+ *
+ * \note           This value starts at an unspecified origin and
+ *                 may wrap around.
+ */
+unsigned long mbedtls_timing_hardclock( void );
+
+/**
+ * \brief          Return the elapsed time in milliseconds
  *
  * \param val      points to a timer structure
  * \param reset    If 0, query the elapsed time. Otherwise (re)start the timer.
@@ -83,6 +93,18 @@ extern volatile int mbedtls_timing_alarmed;
  *                 the delay since the first reset.
  */
 unsigned long mbedtls_timing_get_timer( struct mbedtls_timing_hr_time *val, int reset );
+
+/**
+ * \brief          Setup an alarm clock
+ *
+ * \param seconds  delay before the "mbedtls_timing_alarmed" flag is set
+ *                 (must be >=0)
+ *
+ * \warning        Only one alarm at a time  is supported. In a threaded
+ *                 context, this means one for the whole process, not one per
+ *                 thread.
+ */
+void mbedtls_set_alarm( int seconds );
 
 /**
  * \brief          Set a pair of delays to watch
@@ -113,6 +135,15 @@ void mbedtls_timing_set_delay( void *data, uint32_t int_ms, uint32_t fin_ms );
  *                  2 if the final delay is passed.
  */
 int mbedtls_timing_get_delay( void *data );
+
+#if defined(MBEDTLS_SELF_TEST)
+/**
+ * \brief          Checkup routine
+ *
+ * \return         0 if successful, or 1 if a test failed
+ */
+int mbedtls_timing_self_test( int verbose );
+#endif
 
 #ifdef __cplusplus
 }
