42829507fe69330836479436d146b35918b446a7
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2021-06-22 10:20:48 +0200

    Merge branch 'development' into development-restricted
    * development: (236 commits)
      Changing the key length to 32 bytes in one of the PSA cipher setup tests
      Removal of RC4 certs and fixes to docs and tests
      Fix fd range for select on Windows
      Refactor file descriptor checks into a common function
      Removing global variable and moving variant function comment block
      Fix typo in doc'n of session resumption API
      Code review fixes
      Fix warning in some configurations
      Fix cmake build of fuzz_privkey
      Fix async support in ssl_server2
      Improve ChangeLog and migration guide entries
      Use a proper DRBG in programs
      Use the dedicated dummy_random in fuzzing programs
      Fix cmake build of programs
      Add ChangeLog and migration guide entries
      Simplify internal code
      Remove "internal RNG" code from ECP
      Remove config option MBEDTLS_ECP_NO_INTERNAL_RNG
      Add RNG params to private key parsing
      Add RNG parameter to check_pair functions
      ...
    
    Change-Id: I60b760590530bf914043145a12d03355379654a0

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -47,7 +47,6 @@
  *
  * Used in:
  *      library/aria.c
- *      library/timing.c
  *      library/bn_mul.h
  *
  * Required by:
@@ -268,7 +267,7 @@
 /**
  * \def MBEDTLS_TIMING_ALT
  *
- * Uncomment to provide your own alternate implementation for mbedtls_timing_hardclock(),
+ * Uncomment to provide your own alternate implementation for
  * mbedtls_timing_get_timer(), mbedtls_set_alarm(), mbedtls_set/get_delay()
  *
  * Only works if you have MBEDTLS_TIMING_C enabled.
@@ -645,28 +644,6 @@
 #define MBEDTLS_ECP_NIST_OPTIM
 
 /**
- * \def MBEDTLS_ECP_NO_INTERNAL_RNG
- *
- * When this option is disabled, mbedtls_ecp_mul() will make use of an
- * internal RNG when called with a NULL \c f_rng argument, in order to protect
- * against some side-channel attacks.
- *
- * This protection introduces a dependency of the ECP module on one of the
- * DRBG modules. For very constrained implementations that don't require this
- * protection (for example, because you're only doing signature verification,
- * so not manipulating any secret, or because local/physical side-channel
- * attacks are outside your threat model), it might be desirable to get rid of
- * that dependency.
- *
- * \warning Enabling this option makes some uses of ECP vulnerable to some
- * side-channel attacks. Only enable it if you know that's not a problem for
- * your use case.
- *
- * Uncomment this macro to disable some counter-measures in ECP.
- */
-//#define MBEDTLS_ECP_NO_INTERNAL_RNG
-
-/**
  * \def MBEDTLS_ECP_RESTARTABLE
  *
  * Enable "non-blocking" ECC operations that can return early and be resumed.
@@ -1007,8 +984,7 @@
 /**
  * \def MBEDTLS_NO_DEFAULT_ENTROPY_SOURCES
  *
- * Do not add default entropy sources. These are the platform specific
- * or mbedtls_timing_hardclock poll function.
+ * Do not add default entropy sources in mbedtls_entropy_init().
  *
  * This is useful to have more control over the added entropy sources in an
  * application.
