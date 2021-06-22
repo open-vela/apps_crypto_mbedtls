7e208ffd74bf6a38e8439c773e5aa1ed8cfbff8d
openvela-robot <openvela-robot@xiaomi.com>
2021-06-22 13:41:56 +0800

    Document the return type change in the migration guide
    Signed-off-by: Dave Rodgman <dave.rodgman@arm.com>

--- library/ecp.c
+++ library/ecp.c
@@ -349,9 +349,9 @@ int mbedtls_ecp_check_budget( const mbedtls_ecp_group *grp,
  *  - readable name
  *
  * Curves are listed in order: largest curves first, and for a given size,
- * fastest curves first.
+ * fastest curves first. This provides the default order for the SSL module.
  *
- * Reminder: update profiles in x509_crt.c and ssl_tls.c when adding a new curve!
+ * Reminder: update profiles in x509_crt.c when adding a new curves!
  */
 static const mbedtls_ecp_curve_info ecp_supported_curves[] =
 {
