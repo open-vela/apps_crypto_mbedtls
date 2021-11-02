8116f08df8386f09f40b4b9aebb097c5c38c0c5c
openvela-robot <openvela-robot@xiaomi.com>
2021-11-02 10:50:44 +0800

    generate_psa_tests.py: add key generation result to test case argument list, add comments
    Signed-off-by: Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>

--- library/ecdh_misc.h
+++ library/ecdh_misc.h
@@ -36,19 +36,13 @@ int mbedtls_ecdh_setup_no_everest( mbedtls_ecdh_context *ctx,
                                    mbedtls_ecp_group_id grp_id );
 
 /*
- * TLS 1.3 version of mbedtls_ecdh_make_params
+ * TLS 1.3 version of mbedtls_ecdh_make_params in ecdh.h
  */
 int mbedtls_ecdh_tls13_make_params( mbedtls_ecdh_context *ctx, size_t *olen,
-                                    unsigned char *buf, size_t buf_len,
+                                    unsigned char *buf, size_t blen,
                                     int ( *f_rng )( void *, unsigned char *, size_t ),
                                     void *p_rng );
 
-/*
- * TLS 1.3 version of mbedtls_ecdh_read_public
- */
-int mbedtls_ecdh_tls13_read_public( mbedtls_ecdh_context *ctx,
-                                    const unsigned char *buf,
-                                    size_t buf_len );
 
 #endif /* MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL */
 
