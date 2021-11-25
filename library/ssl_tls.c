2fb96d5d94424e286d09564affac3f2d7b8aae18
liujinye <liujinye@xiaomi.com>
2021-11-25 22:02:43 +0800

    Merge pull request #5222 from paul-elliott-arm/fix_test_suite_ssl
    Fix test_suite_ssl compilation errors with GCC11

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -6376,7 +6376,6 @@ static uint16_t ssl_preset_default_sig_algs[] = {
 #if defined(MBEDTLS_X509_RSASSA_PSS_SUPPORT)
     MBEDTLS_TLS13_SIG_RSA_PSS_RSAE_SHA256,
 #endif
-    MBEDTLS_TLS13_SIG_RSA_PKCS1_SHA256,
 
     MBEDTLS_TLS13_SIG_NONE
 };
@@ -6396,7 +6395,6 @@ static uint16_t ssl_preset_suiteb_sig_algs[] = {
 #if defined(MBEDTLS_X509_RSASSA_PSS_SUPPORT)
     MBEDTLS_TLS13_SIG_RSA_PSS_RSAE_SHA256,
 #endif
-    MBEDTLS_TLS13_SIG_RSA_PKCS1_SHA256,
 
     MBEDTLS_TLS13_SIG_NONE
 };
