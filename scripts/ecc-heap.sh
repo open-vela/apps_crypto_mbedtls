9edb4af2c154c7237d045b3211ccddb52885f1b3
liujinye <liujinye@xiaomi.com>
2022-01-06 19:11:28 +0800

    Merge pull request #5388 from bensze01/multipart_aead_compliance_tests
    Fix the multipart AEAD compliance tests

--- scripts/ecc-heap.sh
+++ scripts/ecc-heap.sh
@@ -59,26 +59,19 @@ cat << EOF >$CONFIG_H
 #define MBEDTLS_ECDSA_C
 #define MBEDTLS_ECDH_C
 
-// NIST curves >= 256 bits
+#define MBEDTLS_ECP_DP_SECP192R1_ENABLED
+#define MBEDTLS_ECP_DP_SECP224R1_ENABLED
 #define MBEDTLS_ECP_DP_SECP256R1_ENABLED
 #define MBEDTLS_ECP_DP_SECP384R1_ENABLED
 #define MBEDTLS_ECP_DP_SECP521R1_ENABLED
-// SECP "koblitz-like" curve >= 256 bits
-#define MBEDTLS_ECP_DP_SECP256K1_ENABLED
-// Brainpool curves (no specialised "mod p" routine)
-#define MBEDTLS_ECP_DP_BP256R1_ENABLED
-#define MBEDTLS_ECP_DP_BP384R1_ENABLED
-#define MBEDTLS_ECP_DP_BP512R1_ENABLED
-// Montgomery curves
 #define MBEDTLS_ECP_DP_CURVE25519_ENABLED
-#define MBEDTLS_ECP_DP_CURVE448_ENABLED
 
 //#define MBEDTLS_ECP_WINDOW_SIZE            6
 //#define MBEDTLS_ECP_FIXED_POINT_OPTIM      1
 EOF
 
 for F in 0 1; do
-    for W in 2 3 4; do
+    for W in 2 3 4 5 6; do
         scripts/config.py set MBEDTLS_ECP_WINDOW_SIZE $W
         scripts/config.py set MBEDTLS_ECP_FIXED_POINT_OPTIM $F
         make benchmark >/dev/null 2>&1
