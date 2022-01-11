87398ef95ecdd0713578215d5285a8dec519cd6f
openvela-robot <openvela-robot@xiaomi.com>
2022-01-11 11:08:53 +0800

    fix coding style issues
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- library/psa_crypto_driver_wrappers.c
+++ library/psa_crypto_driver_wrappers.c
@@ -26,7 +26,6 @@
 #include "psa_crypto_driver_wrappers.h"
 #include "psa_crypto_hash.h"
 #include "psa_crypto_mac.h"
-#include "psa_crypto_rsa.h"
 
 #include "mbedtls/platform.h"
 
@@ -874,8 +873,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
     const uint8_t *key_buffer,
     size_t key_buffer_size,
     psa_algorithm_t alg,
-    const uint8_t *iv,
-    size_t iv_length,
     const uint8_t *input,
     size_t input_length,
     uint8_t *output,
@@ -897,8 +894,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
                                                               key_buffer,
                                                               key_buffer_size,
                                                               alg,
-                                                              iv,
-                                                              iv_length,
                                                               input,
                                                               input_length,
                                                               output,
@@ -915,8 +910,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
                                                 key_buffer,
                                                 key_buffer_size,
                                                 alg,
-                                                iv,
-                                                iv_length,
                                                 input,
                                                 input_length,
                                                 output,
@@ -934,8 +927,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
                                                         key_buffer,
                                                         key_buffer_size,
                                                         alg,
-                                                        iv,
-                                                        iv_length,
                                                         input,
                                                         input_length,
                                                         output,
@@ -950,8 +941,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
             (void)key_buffer;
             (void)key_buffer_size;
             (void)alg;
-            (void)iv;
-            (void)iv_length;
             (void)input;
             (void)input_length;
             (void)output;
@@ -2329,181 +2318,6 @@ psa_status_t psa_driver_wrapper_mac_abort(
 #endif /* PSA_CRYPTO_DRIVER_TEST */
 #endif /* PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT */
         default:
-            return( PSA_ERROR_INVALID_ARGUMENT );
-    }
-}
-
-/*
- * Asymmetric cryptography
- */
-psa_status_t psa_driver_wrapper_asymmetric_encrypt(
-    const psa_key_attributes_t *attributes,
-    const uint8_t *key_buffer,
-    size_t key_buffer_size,
-    psa_algorithm_t alg,
-    const uint8_t *input,
-    size_t input_length,
-    const uint8_t *salt,
-    size_t salt_length,
-    uint8_t *output,
-    size_t output_size,
-    size_t *output_length )
-{
-    psa_status_t status = PSA_ERROR_CORRUPTION_DETECTED;
-    psa_key_location_t location =
-        PSA_KEY_LIFETIME_GET_LOCATION( attributes->core.lifetime );
-
-    switch( location )
-    {
-        case PSA_KEY_LOCATION_LOCAL_STORAGE:
-            /* Key is stored in the slot in export representation, so
-             * cycle through all known transparent accelerators */
-#if defined(PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT)
-#if defined(PSA_CRYPTO_DRIVER_TEST)
-            status = mbedtls_test_transparent_asymmetric_encrypt( attributes,
-                                                                  key_buffer,
-                                                                  key_buffer_size,
-                                                                  alg,
-                                                                  input,
-                                                                  input_length,
-                                                                  salt,
-                                                                  salt_length,
-                                                                  output,
-                                                                  output_size,
-                                                                  output_length );
-            /* Declared with fallback == true */
-            if( status != PSA_ERROR_NOT_SUPPORTED )
-                return( status );
-#endif /* PSA_CRYPTO_DRIVER_TEST */
-#endif /* PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT */
-            return( mbedtls_psa_asymmetric_encrypt( attributes,
-                                                    key_buffer,
-                                                    key_buffer_size,
-                                                    alg,
-                                                    input,
-                                                    input_length,
-                                                    salt,
-                                                    salt_length,
-                                                    output,
-                                                    output_size,
-                                                    output_length ) );
-        /* Add cases for opaque driver here */
-#if defined(PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT)
-#if defined(PSA_CRYPTO_DRIVER_TEST)
-        case PSA_CRYPTO_TEST_DRIVER_LOCATION:
-            return( mbedtls_test_opaque_asymmetric_encrypt( attributes,
-                                                            key_buffer,
-                                                            key_buffer_size,
-                                                            alg,
-                                                            input,
-                                                            input_length,
-                                                            salt,
-                                                            salt_length,
-                                                            output,
-                                                            output_size,
-                                                            output_length ) );
-#endif /* PSA_CRYPTO_DRIVER_TEST */
-#endif /* PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT */
-
-        default:
-            /* Key is declared with a lifetime not known to us */
-            (void)status;
-            (void)key_buffer;
-            (void)key_buffer_size;
-            (void)alg;
-            (void)input;
-            (void)input_length;
-            (void)salt;
-            (void)salt_length;
-            (void)output;
-            (void)output_size;
-            (void)output_length;
-            return( PSA_ERROR_INVALID_ARGUMENT );
-    }
-}
-
-psa_status_t psa_driver_wrapper_asymmetric_decrypt(
-    const psa_key_attributes_t *attributes,
-    const uint8_t *key_buffer,
-    size_t key_buffer_size,
-    psa_algorithm_t alg,
-    const uint8_t *input,
-    size_t input_length,
-    const uint8_t *salt,
-    size_t salt_length,
-    uint8_t *output,
-    size_t output_size,
-    size_t *output_length )
-{
-    psa_status_t status = PSA_ERROR_CORRUPTION_DETECTED;
-    psa_key_location_t location =
-        PSA_KEY_LIFETIME_GET_LOCATION( attributes->core.lifetime );
-
-    switch( location )
-    {
-        case PSA_KEY_LOCATION_LOCAL_STORAGE:
-            /* Key is stored in the slot in export representation, so
-             * cycle through all known transparent accelerators */
-#if defined(PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT)
-#if defined(PSA_CRYPTO_DRIVER_TEST)
-            status = mbedtls_test_transparent_asymmetric_decrypt( attributes,
-                                                                  key_buffer,
-                                                                  key_buffer_size,
-                                                                  alg,
-                                                                  input,
-                                                                  input_length,
-                                                                  salt,
-                                                                  salt_length,
-                                                                  output,
-                                                                  output_size,
-                                                                  output_length );
-            /* Declared with fallback == true */
-            if( status != PSA_ERROR_NOT_SUPPORTED )
-                return( status );
-#endif /* PSA_CRYPTO_DRIVER_TEST */
-#endif /* PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT */
-            return( mbedtls_psa_asymmetric_decrypt( attributes,
-                                                    key_buffer,
-                                                    key_buffer_size,
-                                                    alg,
-                                                    input,
-                                                    input_length,
-                                                    salt,
-                                                    salt_length,
-                                                    output,
-                                                    output_size,
-                                                    output_length ) );
-        /* Add cases for opaque driver here */
-#if defined(PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT)
-#if defined(PSA_CRYPTO_DRIVER_TEST)
-        case PSA_CRYPTO_TEST_DRIVER_LOCATION:
-            return( mbedtls_test_opaque_asymmetric_decrypt( attributes,
-                                                            key_buffer,
-                                                            key_buffer_size,
-                                                            alg,
-                                                            input,
-                                                            input_length,
-                                                            salt,
-                                                            salt_length,
-                                                            output,
-                                                            output_size,
-                                                            output_length ) );
-#endif /* PSA_CRYPTO_DRIVER_TEST */
-#endif /* PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT */
-
-        default:
-            /* Key is declared with a lifetime not known to us */
-            (void)status;
-            (void)key_buffer;
-            (void)key_buffer_size;
-            (void)alg;
-            (void)input;
-            (void)input_length;
-            (void)salt;
-            (void)salt_length;
-            (void)output;
-            (void)output_size;
-            (void)output_length;
             return( PSA_ERROR_INVALID_ARGUMENT );
     }
 }
