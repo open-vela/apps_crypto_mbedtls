9e8391ff94d8a0d7bf99adbbfdc30caa1924da69
openvela-robot <openvela-robot@xiaomi.com>
2022-01-13 16:15:25 +0800

    fix comment issue
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- library/psa_crypto_driver_wrappers.c
+++ library/psa_crypto_driver_wrappers.c
@@ -873,8 +873,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
     const uint8_t *key_buffer,
     size_t key_buffer_size,
     psa_algorithm_t alg,
-    const uint8_t *iv,
-    size_t iv_length,
     const uint8_t *input,
     size_t input_length,
     uint8_t *output,
@@ -896,8 +894,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
                                                               key_buffer,
                                                               key_buffer_size,
                                                               alg,
-                                                              iv,
-                                                              iv_length,
                                                               input,
                                                               input_length,
                                                               output,
@@ -914,8 +910,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
                                                 key_buffer,
                                                 key_buffer_size,
                                                 alg,
-                                                iv,
-                                                iv_length,
                                                 input,
                                                 input_length,
                                                 output,
@@ -933,8 +927,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
                                                         key_buffer,
                                                         key_buffer_size,
                                                         alg,
-                                                        iv,
-                                                        iv_length,
                                                         input,
                                                         input_length,
                                                         output,
@@ -949,8 +941,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
             (void)key_buffer;
             (void)key_buffer_size;
             (void)alg;
-            (void)iv;
-            (void)iv_length;
             (void)input;
             (void)input_length;
             (void)output;
