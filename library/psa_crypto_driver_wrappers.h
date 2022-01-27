9b4ca0e128cac30ff0f4448bd9273835d9d0d003
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2022-01-27 11:15:33 +0100

    Improve introductory comments.
    Change-Id: Ic50f1972c2f0787dd56e948fbb08ca5831862607
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- library/psa_crypto_driver_wrappers.h
+++ library/psa_crypto_driver_wrappers.h
@@ -1,6 +1,7 @@
 /*
  *  Function signatures for functionality that can be provided by
  *  cryptographic accelerators.
+ *  Warning: This file will be auto-generated in the future.
  */
 /*  Copyright The Mbed TLS Contributors
  *  SPDX-License-Identifier: Apache-2.0
@@ -23,12 +24,6 @@
 
 #include "psa/crypto.h"
 #include "psa/crypto_driver_common.h"
-
-/*
- * Initialization and termination functions
- */
-psa_status_t psa_driver_wrapper_init( void );
-void psa_driver_wrapper_free( void );
 
 /*
  * Signature functions
@@ -118,8 +113,6 @@ psa_status_t psa_driver_wrapper_cipher_encrypt(
     const uint8_t *key_buffer,
     size_t key_buffer_size,
     psa_algorithm_t alg,
-    const uint8_t *iv,
-    size_t iv_length,
     const uint8_t *input,
     size_t input_length,
     uint8_t *output,
@@ -225,10 +218,6 @@ psa_status_t psa_driver_wrapper_aead_decrypt(
     const uint8_t *additional_data, size_t additional_data_length,
     const uint8_t *ciphertext, size_t ciphertext_length,
     uint8_t *plaintext, size_t plaintext_size, size_t *plaintext_length );
-
-psa_status_t psa_driver_get_tag_len(
-    psa_aead_operation_t *operation,
-    uint8_t *tag_len );
 
 psa_status_t psa_driver_wrapper_aead_encrypt_setup(
     psa_aead_operation_t *operation,
