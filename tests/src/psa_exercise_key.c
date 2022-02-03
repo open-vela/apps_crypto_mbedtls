123e356600929063b15d359bb9df944b557245de
openvela-robot <openvela-robot@xiaomi.com>
2022-02-03 09:54:34 +0800

    ssl_msg.c: add debug code for psa failures
    Signed-off-by: Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>

--- tests/src/psa_exercise_key.c
+++ tests/src/psa_exercise_key.c
@@ -764,7 +764,6 @@ int mbedtls_test_psa_exported_key_sanity_check(
 #endif /* MBEDTLS_ECP_C */
 
     {
-        (void) exported;
         TEST_ASSERT( ! "Sanity check not implemented for this key type" );
     }
 
