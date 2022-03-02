a6e9bc39c00baddf99b900844b674bcb63ef3cbc
openvela-robot <openvela-robot@xiaomi.com>
2022-03-02 08:45:56 +0800

    Fix code style (indentation) in ssl_tls13_generate_and_write_ecdh_key_exchange()
    Signed-off-by: Przemek Stekiel <przemyslaw.stekiel@mobica.com>

--- library/ssl_debug_helpers.h
+++ library/ssl_debug_helpers.h
@@ -33,8 +33,6 @@
 
 const char *mbedtls_ssl_states_str( mbedtls_ssl_states in );
 
-const char *mbedtls_ssl_protocol_version_str( mbedtls_ssl_protocol_version in );
-
 const char *mbedtls_tls_prf_types_str( mbedtls_tls_prf_types in );
 
 const char *mbedtls_ssl_key_export_type_str( mbedtls_ssl_key_export_type in );
