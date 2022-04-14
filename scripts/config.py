bda7a8c4165f0e4fc3e4195325e0ebb3512cf900
Gilles Peskine <Gilles.Peskine@arm.com>
2022-04-14 12:44:01 +0200

    Wording improvement
    Change-Id: I5bac18e34a2ec3f804a978cf35146602f845f7dc
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- scripts/config.py
+++ scripts/config.py
@@ -161,8 +161,16 @@ def is_full_section(section):
     return section.endswith('support') or section.endswith('modules')
 
 def realfull_adapter(_name, active, section):
-    """Activate all symbols found in the system and feature sections."""
-    if not is_full_section(section):
+    """Activate all symbols found in the global and boolean feature sections.
+
+    This is intended for building the documentation, including the
+    documentation of settings that are activated by defining an optional
+    preprocessor macro.
+
+    Do not activate definitions in the section containing symbols that are
+    supposed to be defined and documented in their own module.
+    """
+    if section == 'Module configuration options':
         return active
     return True
 
@@ -198,8 +206,7 @@ EXCLUDE_FROM_FULL = frozenset([
     'MBEDTLS_PSA_CRYPTO_SPM', # platform dependency (PSA SPM)
     'MBEDTLS_PSA_INJECT_ENTROPY', # build dependency (hook functions)
     'MBEDTLS_RSA_NO_CRT', # influences the use of RSA in X.509 and TLS
-    'MBEDTLS_SHA256_USE_A64_CRYPTO_ONLY', # interacts with *_USE_A64_CRYPTO_IF_PRESENT
-    'MBEDTLS_SHA512_USE_A64_CRYPTO_ONLY', # interacts with *_USE_A64_CRYPTO_IF_PRESENT
+    'MBEDTLS_SHA256_USE_A64_CRYPTO_ONLY', # interacts with *_USE_A64_CRYPTO_ONLY
     'MBEDTLS_TEST_CONSTANT_FLOW_MEMSAN', # build dependency (clang+memsan)
     'MBEDTLS_TEST_CONSTANT_FLOW_VALGRIND', # build dependency (valgrind headers)
     'MBEDTLS_X509_REMOVE_INFO', # removes a feature
@@ -273,21 +280,6 @@ def baremetal_adapter(name, active, section):
         # No OS-provided entropy source
         return True
     return include_in_full(name) and keep_in_baremetal(name)
-
-# This set contains options that are mostly for debugging or test purposes,
-# and therefore should be excluded when doing code size measurements.
-# Options that are their own module (such as MBEDTLS_ERROR_C) are not listed
-# and therefore will be included when doing code size measurements.
-EXCLUDE_FOR_SIZE = frozenset([
-    'MBEDTLS_DEBUG_C', # large code size increase in TLS
-    'MBEDTLS_SELF_TEST', # increases the size of many modules
-    'MBEDTLS_TEST_HOOKS', # only useful with the hosted test framework, increases code size
-])
-
-def baremetal_size_adapter(name, active, section):
-    if name in EXCLUDE_FOR_SIZE:
-        return False
-    return baremetal_adapter(name, active, section)
 
 def include_in_crypto(name):
     """Rules for symbols in a crypto configuration."""
@@ -501,9 +493,6 @@ if __name__ == '__main__':
         add_adapter('baremetal', baremetal_adapter,
                     """Like full, but exclude features that require platform
                     features such as file input-output.""")
-        add_adapter('baremetal_size', baremetal_size_adapter,
-                    """Like baremetal, but exclude debugging features.
-                    Useful for code size measurements.""")
         add_adapter('full', full_adapter,
                     """Uncomment most features.
                     Exclude alternative implementations and platform support
