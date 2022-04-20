05ab73fcc7fa148b9e672d2f871d334362853f73
openvela-robot <openvela-robot@xiaomi.com>
2022-04-20 17:07:52 +0800

    cipher_alg_without_iv: generalized to also do decryption
    Test set_iv/generate_iv after decrypt_setup. Test successful decryption.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- library/bignum.c
+++ library/bignum.c
@@ -1377,22 +1377,54 @@ mbedtls_mpi_uint mbedtls_mpi_core_mla( mbedtls_mpi_uint *d, size_t d_len,
     mbedtls_mpi_uint c = 0; /* carry */
     size_t excess_len = d_len - s_len;
 
-    size_t steps_x8 = s_len / 8;
-    size_t steps_x1 = s_len & 7;
-
-    while( steps_x8-- )
+#if defined(MULADDC_HUIT)
+    for( ; s_len >= 8; s_len -= 8 )
     {
-        MULADDC_X8_INIT
-        MULADDC_X8_CORE
-        MULADDC_X8_STOP
+        MULADDC_INIT
+        MULADDC_HUIT
+        MULADDC_STOP
     }
 
-    while( steps_x1-- )
+    for( ; s_len > 0; s_len-- )
     {
-        MULADDC_X1_INIT
-        MULADDC_X1_CORE
-        MULADDC_X1_STOP
+        MULADDC_INIT
+        MULADDC_CORE
+        MULADDC_STOP
     }
+#else /* MULADDC_HUIT */
+    for( ; s_len >= 16; s_len -= 16 )
+    {
+        MULADDC_INIT
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_CORE   MULADDC_CORE
+
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_STOP
+    }
+
+    for( ; s_len >= 8; s_len -= 8 )
+    {
+        MULADDC_INIT
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_CORE   MULADDC_CORE
+
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_CORE   MULADDC_CORE
+        MULADDC_STOP
+    }
+
+    for( ; s_len > 0; s_len-- )
+    {
+        MULADDC_INIT
+        MULADDC_CORE
+        MULADDC_STOP
+    }
+#endif /* MULADDC_HUIT */
 
     while( excess_len-- )
     {
