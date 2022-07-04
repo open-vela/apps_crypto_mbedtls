c249338e6466b64bf5041d3671f505aec9b2379c
openvela-robot <openvela-robot@xiaomi.com>
2022-07-04 17:59:00 +0800

    Merge pull request #5757 from mpg/update-doc-use-psa
    Update "use PSA" documentation (inc. strategy)

--- library/ssl_cache.c
+++ library/ssl_cache.c
@@ -312,11 +312,7 @@ exit:
 #endif
 
     if( session_serialized != NULL )
-    {
         mbedtls_platform_zeroize( session_serialized, session_serialized_len );
-        mbedtls_free( session_serialized );
-        session_serialized = NULL;
-    }
 
     return( ret );
 }
