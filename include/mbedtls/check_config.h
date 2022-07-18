485cc3802b07b29a40efca12186119e593a4a538
openvela-robot <openvela-robot@xiaomi.com>
2022-07-18 11:34:24 +0800

    Merge pull request #6078 from yuhaoth/pr/add-tls13-paser-psk-kex-mode-ext
    TLS 1.3: PSK: Add parser of psk kex mode ext on server side

--- include/mbedtls/check_config.h
+++ include/mbedtls/check_config.h
@@ -162,6 +162,10 @@
 #error "MBEDTLS_PKCS12_C defined, but not all prerequisites"
 #endif
 
+#if defined(MBEDTLS_PKCS1_V15) && !defined(MBEDTLS_MD_C)
+#error "MBEDTLS_PKCS1_V15 defined, but not all prerequisites"
+#endif
+
 #if defined(MBEDTLS_PKCS1_V21) && !defined(MBEDTLS_MD_C)
 #error "MBEDTLS_PKCS1_V21 defined, but not all prerequisites"
 #endif
@@ -355,7 +359,7 @@
 #endif
 
 #if defined(MBEDTLS_PK_C) && \
-    !defined(MBEDTLS_RSA_C) && !defined(MBEDTLS_ECP_C)
+    ( !defined(MBEDTLS_MD_C) || ( !defined(MBEDTLS_RSA_C) && !defined(MBEDTLS_ECP_C) ) )
 #error "MBEDTLS_PK_C defined, but not all prerequisites"
 #endif
 
