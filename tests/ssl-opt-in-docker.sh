a9030db7ba59233d88dded23306f405ae8e70ab5
openvela-robot <openvela-robot@xiaomi.com>
2022-08-11 09:52:18 +0800

    ctr_drbg: remove mbedtls_aes_init call from mbedtls_ctr_drbg_seed
    Since 11e9310 add mbedtls_aes_init call in mbedtls_ctr_drbg_init, it
    should not init aes_ctx again in mbedtls_ctr_drbg_seed.
    
    Signed-off-by: kXuan <kxuanobj@gmail.com>

--- tests/ssl-opt-in-docker.sh
+++ tests/ssl-opt-in-docker.sh
@@ -17,7 +17,7 @@
 # - scripts/docker_env.sh for general Docker prerequisites and other information.
 # - ssl-opt.sh for notes about invocation of that script.
 
-# Copyright (C) 2006-2019, Arm Limited (or its affiliates), All Rights Reserved.
+# Copyright The Mbed TLS Contributors
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -31,8 +31,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# This file is part of Mbed TLS (https://tls.mbed.org)
 
 source tests/scripts/docker_env.sh
 
@@ -44,13 +42,13 @@ esac
 
 case "${GNUTLS_CLI:-default}" in
     "legacy")  export GNUTLS_CLI="/usr/local/gnutls-3.3.8/bin/gnutls-cli";;
-    "next")  export GNUTLS_CLI="/usr/local/gnutls-3.6.5/bin/gnutls-cli";;
+    "next")  export GNUTLS_CLI="/usr/local/gnutls-3.7.2/bin/gnutls-cli";;
     *) ;;
 esac
 
 case "${GNUTLS_SERV:-default}" in
     "legacy")  export GNUTLS_SERV="/usr/local/gnutls-3.3.8/bin/gnutls-serv";;
-    "next")  export GNUTLS_SERV="/usr/local/gnutls-3.6.5/bin/gnutls-serv";;
+    "next")  export GNUTLS_SERV="/usr/local/gnutls-3.7.2/bin/gnutls-serv";;
     *) ;;
 esac
 
