b946f9c014df8a2f697de6eb816bee146a087e23
liujinye <liujinye@xiaomi.com>
2022-09-16 11:27:57 +0800

    fix build fail for test_psa_crypto_config_accel_hash_use_psa
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -52,6 +52,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mbedtls/platform.h"
+#if !defined(MBEDTLS_PLATFORM_C)
+#define mbedtls_calloc calloc
+#define mbedtls_free   free
+#endif
 
 #include "mbedtls/aes.h"
 #include "mbedtls/asn1.h"
