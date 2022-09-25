48cbcc08476f5e9b9d36eb0d301f7838d149d1fa
openvela-robot <openvela-robot@xiaomi.com>
2022-09-26 04:01:06 +0800

    Fix wrongly kex mode fallback issue in psk cases
    Signed-off-by: Xiaokang Qian <xiaokang.qian@arm.com>

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -120,9 +120,6 @@
 # Treat uninitialised variables as errors.
 set -e -o pipefail -u
 
-# Enable ksh/bash extended file matching patterns
-shopt -s extglob
-
 pre_check_environment () {
     if [ -d library -a -d include -a -d tests ]; then :; else
         echo "Must be run from mbed TLS root" >&2
@@ -298,7 +295,7 @@ cleanup()
               -iname CMakeCache.txt -o \
               -path './cmake/*.cmake' \) -exec rm -f {} \+
     # Recover files overwritten by in-tree CMake builds
-    rm -f include/Makefile include/mbedtls/Makefile programs/!(fuzz)/Makefile
+    rm -f include/Makefile include/mbedtls/Makefile programs/*/Makefile
 
     # Remove any artifacts from the component_test_cmake_as_subdirectory test.
     rm -rf programs/test/cmake_subproject/build
@@ -317,9 +314,7 @@ cleanup()
 
     # Restore files that may have been clobbered by the job
     for x in $files_to_back_up; do
-        if [[ -e "$x$backup_suffix" ]]; then
-            cp -p "$x$backup_suffix" "$x"
-        fi
+        cp -p "$x$backup_suffix" "$x"
     done
 }
 
