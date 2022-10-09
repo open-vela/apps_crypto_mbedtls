0d496bf8c60b8ef374085635c9399da7b460ba9c
openvela-robot <openvela-robot@xiaomi.com>
2022-10-09 21:14:09 +0800

    mbedtls_test_read_mpi_core: allow odd number of hex digits
    Test functions must now take a char* argument rather than data_t*. This does
    not affect existing test data.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- library/ssl_misc.h
+++ library/ssl_misc.h
@@ -32,7 +32,7 @@
 #include "mbedtls/psa_util.h"
 #include "hash_info.h"
 #endif
-#include "legacy_or_psa.h"
+#include "mbedtls/legacy_or_psa.h"
 
 #if defined(MBEDTLS_MD5_C)
 #include "mbedtls/md5.h"
@@ -623,6 +623,9 @@ struct mbedtls_ssl_handshake_params
     uint16_t hrr_selected_group;
 #if defined(MBEDTLS_KEY_EXCHANGE_SOME_PSK_ENABLED)
     uint8_t tls13_kex_modes; /*!< Key exchange modes supported by the client */
+#endif
+#if defined(MBEDTLS_SSL_SESSION_TICKETS)
+    uint16_t new_session_tickets_count;         /*!< number of session tickets */
 #endif
 #endif /* MBEDTLS_SSL_SRV_C */
 
