2fe4fb4c4cd839ebb92c75356a90aab8e0d001e9
openvela-robot <openvela-robot@xiaomi.com>
2022-10-13 09:59:52 +0800

    Disable MBEDTLS_SSL_TICKET_C in aead driver test.
    MBEDTLS_SSL_TICKET_C depends now on: MBEDTLS_GCM_C || MBEDTLS_CCM_C || MBEDTLS_CHACHAPOLY_C.
    All features are disabled in this config.
    
    Signed-off-by: Przemek Stekiel <przemyslaw.stekiel@mobica.com>

--- include/mbedtls/check_config.h
+++ include/mbedtls/check_config.h
@@ -874,11 +874,6 @@
 #error "MBEDTLS_SSL_TICKET_C defined, but not all prerequisites"
 #endif
 
-#if defined(MBEDTLS_SSL_TICKET_C) && \
-    !( defined(MBEDTLS_GCM_C) || defined(MBEDTLS_CCM_C) || defined(MBEDTLS_CHACHAPOLY_C) )
-#error "MBEDTLS_SSL_TICKET_C defined, but not all prerequisites"
-#endif
-
 #if defined(MBEDTLS_SSL_TLS1_3_TICKET_NONCE_LENGTH) && \
     MBEDTLS_SSL_TLS1_3_TICKET_NONCE_LENGTH >= 256
 #error "MBEDTLS_SSL_TLS1_3_TICKET_NONCE_LENGTH must be less than 256"
@@ -967,9 +962,7 @@
 #error "MBEDTLS_SSL_VARIABLE_BUFFER_LENGTH defined, but not all prerequisites"
 #endif
 
-#if defined(MBEDTLS_SSL_CONTEXT_SERIALIZATION) && !( defined(MBEDTLS_GCM_C) || defined(MBEDTLS_CCM_C) || defined(MBEDTLS_CHACHAPOLY_C) )
-#error "MBEDTLS_SSL_CONTEXT_SERIALIZATION defined, but not all prerequisites"
-#endif
+
 
 /* Reject attempts to enable options that have been removed and that could
  * cause a build to succeed but with features removed. */
