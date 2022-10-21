f7248c789a40db631c018821d874281bdff0c1b6
openvela-robot <openvela-robot@xiaomi.com>
2022-10-21 19:34:54 +0800

    Improve test component name
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- library/pkwrite.c
+++ library/pkwrite.c
@@ -51,7 +51,13 @@
 #include "psa/crypto.h"
 #include "mbedtls/psa_util.h"
 #endif
+#if defined(MBEDTLS_PLATFORM_C)
 #include "mbedtls/platform.h"
+#else
+#include <stdlib.h>
+#define mbedtls_calloc    calloc
+#define mbedtls_free       free
+#endif
 
 #if defined(MBEDTLS_RSA_C)
 /*
