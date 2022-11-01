30da7701e69444f095da451d7f92ab443f49035a
Werner Lewis <werner.lewis@arm.com>
2022-11-01 13:36:51 +0000

    Use modulus structure in mbedtls_mpi_mod_raw_add
    Signed-off-by: Werner Lewis <werner.lewis@arm.com>

--- library/bignum_mod_raw.c
+++ library/bignum_mod_raw.c
@@ -132,13 +132,12 @@ void mbedtls_mpi_mod_raw_sub( mbedtls_mpi_uint *X,
 void mbedtls_mpi_mod_raw_add( mbedtls_mpi_uint *X,
                               mbedtls_mpi_uint const *A,
                               mbedtls_mpi_uint const *B,
-                              const mbedtls_mpi_uint *N,
-                              size_t limbs )
+                              const mbedtls_mpi_mod_modulus *N )
 {
     size_t carry, borrow = 0;
-    carry  = mbedtls_mpi_core_add( X, A, B, limbs );
-    borrow = mbedtls_mpi_core_sub( X, X, N, limbs);
-    (void) mbedtls_mpi_core_add_if( X, N, limbs, ( carry < borrow ) );
+    carry  = mbedtls_mpi_core_add( X, A, B, N->limbs );
+    borrow = mbedtls_mpi_core_sub( X, X, N->p, N->limbs );
+    (void) mbedtls_mpi_core_add_if( X, N->p, N->limbs, ( carry < borrow ) );
 }
 /* END MERGE SLOT 5 */
 
