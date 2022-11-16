988c22e3426d5ff850ffbce997a9019bd123cc94
Przemek Stekiel <przemyslaw.stekiel@mobica.com>
2022-11-16 12:55:27 +0100

    Disable PSA_WANT_ALG_STREAM_CIPHER, PSA_WANT_ALG_ECB_NO_PADDING also in reference config
    Signed-off-by: Przemek Stekiel <przemyslaw.stekiel@mobica.com>
    
    Change-Id: Iad47eab2689ab4a4937bed17c37bb2ce30d2d3c1
    Signed-off-by: Przemek Stekiel <przemyslaw.stekiel@mobica.com>

--- include/mbedtls/legacy_or_psa.h
+++ include/mbedtls/legacy_or_psa.h
@@ -1,10 +1,7 @@
 /**
  *  Macros to express dependencies for code and tests that may use either the
- *  legacy API or PSA in various builds. This whole header file is currently
- *  for internal use only and both the header file and the macros it defines
- *  may change or be removed without notice.
- */
-/*
+ *  legacy API or PSA in various builds; mostly for internal use.
+ *
  *  Copyright The Mbed TLS Contributors
  *  SPDX-License-Identifier: Apache-2.0
  *
