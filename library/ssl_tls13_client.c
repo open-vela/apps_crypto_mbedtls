89f854b90ffaa8ec94ffa729c6f85321aae39bb8
liujinye <liujinye@xiaomi.com>
2022-11-23 13:32:02 +0800

    Merge pull request #6235 from tom-cosgrove-arm/issue-6231-core-sub-int
    Bignum: extract core_sub_int from the prototype

--- library/ssl_tls13_client.c
+++ library/ssl_tls13_client.c
@@ -1183,11 +1183,11 @@ int mbedtls_ssl_tls13_write_client_hello_exts( mbedtls_ssl_context *ssl,
             return( ret );
         p += ext_len;
 
-        /* Initializes the status to `rejected`. It will be updated to
-         * `accepted` if the EncryptedExtension message contain an early data
-         * indication extension.
+        /* Initializes the status to `indication sent`. It will be updated to
+         * `accepted` or `rejected` depending on whether the EncryptedExtension
+         * message will contain an early data indication extension or not.
          */
-        ssl->early_data_status = MBEDTLS_SSL_EARLY_DATA_STATUS_REJECTED;
+        ssl->early_data_status = MBEDTLS_SSL_EARLY_DATA_STATUS_INDICATION_SENT;
     }
     else
     {
