0bc5ee30240ae88f0bdf0cadf4821be4092cf8ea
Glenn Strauss <gstrauss@gluelogic.com>
2022-12-16 10:47:15 -0500

    comments and whitespace
    Change-Id: Ie6b018bbb03fe2adb205092969eafff16ebf6bb8
    Signed-off-by: Glenn Strauss <gstrauss@gluelogic.com>

--- library/oid.c
+++ library/oid.c
@@ -27,12 +27,16 @@
 #include "mbedtls/rsa.h"
 #include "mbedtls/error.h"
 
-#include "mbedtls/legacy_or_psa.h"
+#include "legacy_or_psa.h"
 
 #include <stdio.h>
 #include <string.h>
 
+#if defined(MBEDTLS_PLATFORM_C)
 #include "mbedtls/platform.h"
+#else
+#define mbedtls_snprintf snprintf
+#endif
 
 /*
  * Macro to automatically add the size of #define'd OIDs
