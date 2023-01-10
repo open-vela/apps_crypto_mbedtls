8adcd3426900d32ff6fa7a7488b84b1c1b939803
liujinye <liujinye@xiaomi.com>
2023-01-10 10:01:17 +0800

    Merge pull request #6863 from valeriosetti/issue6830
    Remove uses of mbedtls_ecp_curve_info in TLS (with USE_PSA)

--- programs/test/benchmark.c
+++ programs/test/benchmark.c
@@ -61,9 +61,11 @@ int main( void )
 
 #include "mbedtls/error.h"
 
+/* *INDENT-OFF* */
 #ifndef asm
 #define asm __asm
 #endif
+/* *INDENT-ON* */
 
 #if defined(_WIN32) && !defined(EFIX64) && !defined(EFI32)
 
@@ -398,7 +400,7 @@ static unsigned long mbedtls_timing_hardclock( void )
     }
 
     gettimeofday( &tv_cur, NULL );
-    return( ( tv_cur.tv_sec  - tv_init.tv_sec  ) * 1000000
+    return( ( tv_cur.tv_sec  - tv_init.tv_sec  ) * 1000000U
           + ( tv_cur.tv_usec - tv_init.tv_usec ) );
 }
 #endif /* !HAVE_HARDCLOCK */
