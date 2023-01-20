b9a0d687096b4a0c6114711a88141940bc72b587
liujinye <liujinye@xiaomi.com>
2023-01-20 12:45:13 +0800

    Remove third argument passed to run_client in compat.sh
    The argument passed to translate_ciphers.py is calculated from $1 in
    run_client instead of passed as third argument.
    
    Signed-off-by: Yanray Wang <yanray.wang@arm.com>

--- library/pkparse.c
+++ library/pkparse.c
@@ -429,11 +429,7 @@ static int pk_group_id_from_specified(const mbedtls_asn1_buf *params,
     ret = pk_group_id_from_group(&grp, grp_id);
 
 cleanup:
-    mbedtls_mpi_free(&grp.N);
-    mbedtls_mpi_free(&grp.P);
-    mbedtls_mpi_free(&grp.A);
-    mbedtls_mpi_free(&grp.B);
-    mbedtls_ecp_point_free(&grp.G);
+    mbedtls_ecp_group_free(&grp);
 
     return ret;
 }
