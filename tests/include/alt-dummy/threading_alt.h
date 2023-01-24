6b12647205ee3516b785a23156b820f925fc6425
Andrzej Kurek <andrzej.kurek@arm.com>
2023-01-24 07:40:42 -0500

    depends.py: improve expected argument type
    Requested config option can be either boolean or a string.
    Change-Id: I45b6ea72a8168d01d3a623867ebec8793fd2dbd5
    Signed-off-by: Andrzej Kurek <andrzej.kurek@arm.com>

--- tests/include/alt-dummy/threading_alt.h
+++ tests/include/alt-dummy/threading_alt.h
@@ -19,7 +19,8 @@
 #ifndef THREADING_ALT_H
 #define THREADING_ALT_H
 
-typedef struct mbedtls_threading_mutex_t {
+typedef struct mbedtls_threading_mutex_t
+{
     int dummy;
 } mbedtls_threading_mutex_t;
 
