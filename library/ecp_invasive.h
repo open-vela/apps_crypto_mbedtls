2250d1f612914e85ab8ff86b9761e0cf41d0b464
liujinye <liujinye@xiaomi.com>
2023-01-31 21:22:57 +0800

    Merge pull request #6964 from davidhorstmann-arm/code-style-improvements
    Improvements to code style script

--- library/ecp_invasive.h
+++ library/ecp_invasive.h
@@ -76,19 +76,6 @@ int mbedtls_ecp_gen_privkey_mx(size_t n_bits,
 
 #endif /* MBEDTLS_ECP_MONTGOMERY_ENABLED */
 
-#if defined(MBEDTLS_ECP_DP_SECP192R1_ENABLED)
-
-/** Fast quasi-reduction modulo p192 (FIPS 186-3 D.2.1)
- *
- * \param[in,out]   Np  The address of the MPI to be converted.
- *                      Must have the double the limbs than \p Nn.
- * \param[in]       Nn  The address of the modulus.
- */
-MBEDTLS_STATIC_TESTABLE
-int mbedtls_ecp_mod_p192_raw(mbedtls_mpi_uint *Np, size_t Nn);
-
-#endif /* MBEDTLS_ECP_DP_SECP192R1_ENABLED */
-
 #endif /* MBEDTLS_TEST_HOOKS && MBEDTLS_ECP_C */
 
 #endif /* MBEDTLS_ECP_INVASIVE_H */
