90c33225acbafbbc9038face6a8e73d3c22b0509
makejian <makejian@xiaomi.com>
2025-08-07 20:55:49 +0800

    mbedtls/gcm: use_len could reach 16
    Signed-off-by: makejian <makejian@xiaomi.com>

--- library/gcm.c
+++ library/gcm.c
@@ -400,7 +400,7 @@ static int gcm_mask(mbedtls_gcm_context *ctx,
     size_t olen = 0;
     int ret = MBEDTLS_ERR_ERROR_CORRUPTION_DETECTED;
 
-    if (offset + use_len >= 16)
+    if (offset + use_len > 16)
       {
         return MBEDTLS_ERR_GCM_BAD_INPUT;
       }
