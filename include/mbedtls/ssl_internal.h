hanno-becker/mbedtls

Add tests for record encryption/decryption
This commit adds tests exercising mutually inverse pairs of
record encryption and decryption transformations for the various
transformation types allowed in TLS: Stream, CBC, and AEAD.

include/mbedtls/ssl_internal.h
@@ -786,4 +786,14 @@ int mbedtls_ssl_get_key_exchange_md_tls1_2( mbedtls_ssl_context *ssl,
 }
 #endif
 
+void ssl_transform_init( mbedtls_ssl_transform *transform );
+int ssl_encrypt_buf( mbedtls_ssl_context *ssl,
+                     mbedtls_ssl_transform *transform,
+                     mbedtls_record *rec,
+                     int (*f_rng)(void *, unsigned char *, size_t),
+                     void *p_rng );
+int ssl_decrypt_buf( mbedtls_ssl_context *ssl,
+                     mbedtls_ssl_transform *transform,
+                     mbedtls_record *rec );
+
 #endif /* ssl_internal.h */

open-vela/apps_crypto_mbedtls

include/mbedtls/ssl_internal.h
@@ -950,4 +950,14 @@ int mbedtls_ssl_get_key_exchange_md_tls1_2( mbedtls_ssl_context *ssl,
 }
 #endif
 
+void mbedtls_ssl_transform_init( mbedtls_ssl_transform *transform );
+int mbedtls_ssl_encrypt_buf( mbedtls_ssl_context *ssl,
+                             mbedtls_ssl_transform *transform,
+                             mbedtls_record *rec,
+                             int (*f_rng)(void *, unsigned char *, size_t),
+                             void *p_rng );
+int mbedtls_ssl_decrypt_buf( mbedtls_ssl_context *ssl,
+                             mbedtls_ssl_transform *transform,
+                             mbedtls_record *rec );
+
 #endif /* ssl_internal.h */