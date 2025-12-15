Mbed-TLS/mbedtls

Use more specific name in debug message for testing
While 'session hash' is currently unique, so suitable to prove that the
intended code path has been taken, it's a generic enough phrase that in the
future we might add other debug messages containing it in completely unrelated
code paths. In order to future-proof the accuracy of the test, let's use a
more specific string.

library/ssl_tls.c
@@ -1143,7 +1143,8 @@ static int ssl_compute_master( mbedtls_ssl_handshake_params *handshake,
 
         handshake->calc_verify( ssl, session_hash, &hash_len );
 
-        MBEDTLS_SSL_DEBUG_BUF( 3, "session hash", session_hash, hash_len );
+        MBEDTLS_SSL_DEBUG_BUF( 3, "session hash for extended master secret",
+                                  session_hash, hash_len );
 
         ret = handshake->tls_prf( handshake->premaster, handshake->pmslen,
                                   "extended master secret",

open-vela/apps_crypto_mbedtls

library/ssl_tls.c
@@ -1656,7 +1656,8 @@ static int ssl_compute_master( mbedtls_ssl_handshake_params *handshake,
         salt = session_hash;
         handshake->calc_verify( ssl, session_hash, &salt_len );
 
-        MBEDTLS_SSL_DEBUG_BUF( 3, "session hash", session_hash, salt_len );
+        MBEDTLS_SSL_DEBUG_BUF( 3, "session hash for extended master secret",
+                                  session_hash, salt_len );
     }
 #endif /* MBEDTLS_SSL_EXTENDED_MS_ENABLED */
 